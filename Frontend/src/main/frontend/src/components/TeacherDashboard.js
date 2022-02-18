53bc00ba9db47d8a858d840cc4807d6bb149bcd8
Noah S. Newton <89654929+noahnewt@users.noreply.github.com>
2022-02-18 16:15:44 -0500

    Added Side Bar to Teacher Dashboard

--- Frontend/src/main/frontend/src/components/TeacherDashboard.js
+++ Frontend/src/main/frontend/src/components/TeacherDashboard.js
@@ -1,1 +1,14 @@
 import React from 'react';
+
+import Sidebar from "./Sidebar";
+
+function TeacherDashboard(){
+    return (
+        <div className={"TeacherDashboard"}>
+            <Sidebar />
+        </div>
+
+    );
+}
+
+export default  TeacherDashboard;
