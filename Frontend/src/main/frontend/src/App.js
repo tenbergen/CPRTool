578aef2cde07b63d32e57561560eda649355f440
Noah S. Newton <89654929+noahnewt@users.noreply.github.com>
2022-02-18 22:55:29 -0500

    Cleaned up Teacher Dashboard, Added Styling and Button

--- Frontend/src/main/frontend/src/App.js
+++ Frontend/src/main/frontend/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import TeacherDashboard from "./components/TeacherDashboard";
 
 function App() {
   return (
     <div className="App">
-
+        <TeacherDashboard />
     </div>
   );
 }
--- Frontend/src/main/frontend/src/components/TeacherDashboard.js
+++ Frontend/src/main/frontend/src/components/TeacherDashboard.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
 import Sidebar from "./Sidebar";
+import "./TeacherDashboard.css"
 
 function TeacherDashboard(){
     return (
         <div className={"TeacherDashboard"}>
-            <Sidebar />
+                <Sidebar />
+            <div id="teacher">
+                <h1>
+                    Teacher Dashboard
+                </h1>
+                <div id="addClass">
+                    <button id="addButton">
+                        Add Class
+                    </button>
+                </div>
+            </div>
+
         </div>
 
     );
