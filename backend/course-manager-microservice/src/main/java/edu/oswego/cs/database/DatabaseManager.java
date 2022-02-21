d900b43d79294defac7e56c01e62d523a42f7cab
Gregory Maldonado <gmaldona@oswego.edu>
2022-02-21 01:32:48 -0500

    Database managewr

--- backend/course-manager-microservice/src/main/java/edu/oswego/cs/database/DatabaseManager.java
+++ backend/course-manager-microservice/src/main/java/edu/oswego/cs/database/DatabaseManager.java
@@ -12,11 +12,11 @@ import javax.enterprise.inject.Disposes;
 @ApplicationScoped
 public class DatabaseManager {
 
-    String hostname = "moxie.cs.oswego.edu";
-    int port = 27027;
-    String database = "cpr";
-    String user = "root";
-    String password = "toor";
+    String hostname = "moxie.cs.oswego.edu";//System.getenv("MONGO_HOSTNAME");
+    int port = 27027;//Integer.parseInt(System.getenv("MONGO_PORT"));
+    String database = "cpr";//System.getenv("MONGO_DATABASE");
+    String user = "root";//System.getenv("MONGO_USERNAME");
+    String password = "toor";//System.getenv("MONGO_PASSWORD");
 
     public MongoDatabase getDB() {
         MongoCredential credentials = MongoCredential.createCredential(user, database, password.toCharArray());
