94a33e4acba60eed73b190e1eb3501870727174d
Mint <developer@chocomint.dev>
2022-03-03 23:35:16 -0500

    Removed port from domain

--- backend/login-microservice/src/main/java/edu/oswego/cs/rest/controllers/servlets/LogoutServlet.java
+++ backend/login-microservice/src/main/java/edu/oswego/cs/rest/controllers/servlets/LogoutServlet.java
@@ -13,7 +13,7 @@ public class LogoutServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         request.getSession().invalidate(); // using this would invalidate the user's session
 
-        response.sendRedirect("http://moxie.cs.oswego.edu:13129");
+        response.sendRedirect("http://moxie.cs.oswego.edu");
 
         //this right here would completely log the users out with all google devices
         // response.sendRedirect("https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=http://localhost:3000");
