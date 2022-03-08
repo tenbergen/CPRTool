a352751e909703aac4fa00e359a4b127b06b2f5f
Joshua Smith <jsmith60@oswego.edu>
2022-03-08 16:38:57 -0500

    update login page and css

--- frontend/src/main/frontend/src/pages/LoginPage.js
+++ frontend/src/main/frontend/src/pages/LoginPage.js
@@ -4,34 +4,36 @@ import { useDispatch } from 'react-redux';
 import { authenticateUser } from '../redux/features/authSlice';
 
 function LoginPage() {
-  const loginURL = `${process.env.REACT_APP_LOCAL}/login`
-  const dispatch = useDispatch();
-  const queryParams = new URLSearchParams(window.location.search);
+    const loginURL = `${process.env.REACT_APP_LOCAL}/login`
+    const dispatch = useDispatch();
+    const queryParams = new URLSearchParams(window.location.search);
 
-  useEffect(() => {
-    const token = queryParams.get('token');
-    if (token != null) {
-      localStorage.setItem("jwt_token", token)
-      dispatch(authenticateUser())
-    }
-  },[])
+    useEffect(() => {
+        const token = queryParams.get('token');
+        if (token != null) {
+            localStorage.setItem("jwt_token", token)
+            dispatch(authenticateUser())
+        }
+    },[])
 
-  return (
-    <div id='box'>
-      <div className='googleButton'>
-        <a href={loginURL}>
-          <button type='button'>
-            <img
-                alt="google login img"
-                className={'google'}
-                src={require('./img/Google__G__Logo.svg.png')}
-            />{' '}
-            Login With Google
-          </button>
-        </a>
-      </div>
-    </div>
-  );
+    return (
+        <div id='bigBox'>
+                <div id='box'>
+                    <div className='welcome'>Welcome!</div>
+                    <a href={loginURL}>
+                        <button className='googleButton' type='login' onClick={handleClick}>
+                            <img
+                                alt="google login img"
+                                className={'google'}
+                                src={require('./img/google_logo.png')}
+                            />{' '}
+                            Login With Google
+                        </button>
+                    </a>
+                    <a href="" className="started">Get started as an Instructor</a>
+                </div>
+        </div>
+    );
 }
 
 export default LoginPage;
