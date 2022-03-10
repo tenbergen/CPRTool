b23f4c84e08ae90a7255399df1404603940ffaa6
Noah S. Newton <89654929+noahnewt@users.noreply.github.com>
2022-03-10 17:39:27 -0500

    Added Student Dashboard Todo Page and added Routing

---
+++ frontend/src/main/frontend/src/pages/StudentDashboardPage.js
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react';
+import { Link } from "react-router-dom";
+import SidebarComponent from "../components/SidebarComponent";
+import "./styles/StudentDashboardStyle.css"
+import { useDispatch, useSelector } from "react-redux";
+import { getCoursesAsync } from "../redux/features/courseSlice";
+
+function StudentDashboardPage() {
+    const dispatch = useDispatch()
+    const courses = useSelector((state) => state.courses)
+    const user = useSelector((state) => state.auth.user)
+
+    useEffect( ()=> {
+       dispatch(getCoursesAsync())
+    },[]);
+
+    if (!courses) {
+        return <div><h1>LOADING</h1></div>
+    }
+
+    return (
+        <div className={"StudentDashboard"}>
+            <SidebarComponent />
+            <div id="student">
+                <h1> Hello {user}</h1>
+                <div>
+                    {courses.map(course =>
+                        <Link to="/todoCourse" state={{from: course}}>
+                            <li className="courseListItem" value={course}>{course.CourseName}</li>
+                        </Link>)}
+                </div>
+            </div>
+        </div>
+    );
+
+
+}
+
+export default StudentDashboardPage;
--- frontend/src/main/frontend/src/routing/RouterHandler.js
+++ frontend/src/main/frontend/src/routing/RouterHandler.js
@@ -2,9 +2,12 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from '../App';
 import CreateCoursePage from '../pages/CreateCoursePage';
 import TeacherDashboardPage from '../pages/TeacherDashboardPage';
+import StudentDashboardPage from '../pages/StudentDashboardPage'
 import EditCoursePage from '../pages/EditCoursePage';
+import TodoPage from "../pages/TodoPage";
 import React from 'react';
 import UnauthedErrorPage from '../pages/UnauthedErrorPage';
+
 
 const RouterHandler = () => {
   return (
@@ -13,6 +16,8 @@ const RouterHandler = () => {
         <Route path='/' element={<App />} />
         <Route path='createCourse' element={<CreateCoursePage />} />
         <Route path='teacherDashboard' element={<TeacherDashboardPage />} />
+        <Route path='studentDashboard' element={<StudentDashboardPage />} />
+        <Route path='todoCourse' element={<TodoPage />} />
         <Route path='editCourse' element={<EditCoursePage />} />
         <Route path='unauthenticated' element={<UnauthedErrorPage />} />
         </Routes>
