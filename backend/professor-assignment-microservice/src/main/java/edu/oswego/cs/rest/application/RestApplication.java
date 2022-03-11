8bc4524ddc3bbd1510864064aedb38fa9e3a60bf
Growing-Pains <davisnicholasbusiness@gmail.com>
2022-03-10 19:20:24 -0500

    deleted duplicate files

--- backend/professor-assignment-microservice/src/main/java/edu/oswego/cs/rest/application/RestApplication.java
+++
@@ -1,8 +0,0 @@
-package edu.oswego.cs.rest.application;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("professor")
-public class RestApplication extends Application {
-}
