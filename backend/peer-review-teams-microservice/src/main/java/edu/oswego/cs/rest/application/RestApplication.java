dfcd9e473170f8fa7b4580aabebeadd67b8c006c
Logan Nguyen <Lgad31vn@gmail.com>
2022-03-11 23:53:59 -0500

    update ports, file structures

--- backend/peer-review-teams-microservice/src/main/java/edu/oswego/cs/rest/application/RestApplication.java
+++ backend/peer-review-teams-microservice/src/main/java/edu/oswego/cs/rest/application/RestApplication.java
@@ -1,9 +1,9 @@
-package edu.oswego.cs.rest;
+package edu.oswego.cs.rest.application;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/api")
+@ApplicationPath("/teams")
 public class RestApplication extends Application {
 
 }
