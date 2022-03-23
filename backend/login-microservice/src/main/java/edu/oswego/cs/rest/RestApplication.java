fdc4af04db4b217790b4a089698578b67fd0e303
Logan Nguyen <Lgad31vn@gmail.com>
2022-03-23 17:23:02 -0400

    Init new login-ms-2.0

---
+++ backend/login-microservice/src/main/java/edu/oswego/cs/rest/RestApplication.java
@@ -0,0 +1,9 @@
+package edu.oswego.cs.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class RestApplication extends Application {
+
+}
