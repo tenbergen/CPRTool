755436eb5205c0fe0763a7baed0718fa205e50a6
Mint <developer@chocomint.dev>
2022-03-24 20:14:57 -0400

    Added 5th database
    Added 5th database

--- backend/course-viewer-microservice/src/main/java/edu/oswego/cs/database/DatabaseManager.java
+++ backend/course-viewer-microservice/src/main/java/edu/oswego/cs/database/DatabaseManager.java
@@ -13,7 +13,8 @@ public class DatabaseManager {
     int studentPort = Integer.parseInt(System.getenv("MONGO_PORT"));
     int professorPort = Integer.parseInt(System.getenv("MONGO2_PORT"));
     int coursePort = Integer.parseInt(System.getenv("MONGO3_PORT"));
-    int teamPort = Integer.parseInt(System.getenv("MONGO4_PORT"));
+    int assignmentPort = Integer.parseInt(System.getenv("MONGO4_PORT"));
+    int teamPort = Integer.parseInt(System.getenv("MONGO5_PORT"));
     String hostname = System.getenv("MONGO_HOSTNAME");
     String mongoDatabase = System.getenv("MONGO_DATABASE");
     String mongoUser = System.getenv("MONGO_USERNAME");
@@ -43,6 +44,16 @@ public class DatabaseManager {
         MongoCredential credentials = MongoCredential.createCredential(mongoUser, mongoDatabase, mongoPassword.toCharArray());
         MongoClient client = new MongoClient(
                 new ServerAddress(hostname, coursePort),
+                credentials,
+                new MongoClientOptions.Builder().build()
+        );
+        return client.getDatabase(mongoDatabase);
+    }
+
+    public MongoDatabase getAssignmentDB() {
+        MongoCredential credentials = MongoCredential.createCredential(mongoUser, mongoDatabase, mongoPassword.toCharArray());
+        MongoClient client = new MongoClient(
+                new ServerAddress(hostname, assignmentPort),
                 credentials,
                 new MongoClientOptions.Builder().build()
         );
