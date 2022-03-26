539a58f2d517085c21c4b5e6f6cafb984f16d5a0
Joshua Smith <jsmith60@oswego.edu>
2022-03-26 18:16:54 -0400

    update roster and css

--- frontend/src/main/frontend/src/components/RosterComponent.js
+++ frontend/src/main/frontend/src/components/RosterComponent.js
@@ -1,13 +1,16 @@
-import {useLocation} from "react-router-dom";
 import {useState} from "react";
 import axios from "axios";
 import "./styles/Roster.css"
+import {useSelector} from "react-redux";
 
 
 const RosterComponent = () => {
-    const addStudentUrl = `${window.location.protocol}//${window.location.host}/`
-    // const {from} = useLocation().state
-    // console.log(from)
+    const addStudentUrl = `${window.location.protocol}//${window.location.host}/manage/professor/courses/course/student/add`
+    // const addStudentUrl = `http://moxie.cs.oswego.edu:13125/manage/professor/courses/course/student/add`
+    const deleteStudentUrl = `${window.location.protocol}//${window.location.host}/manage/professor/courses/course/student/delete`
+    // const deleteStudentUrl = `http://moxie.cs.oswego.edu:13125/manage/professor/courses/course/student/delete`
+    const currentCourse = useSelector((state) => state.courses.currentCourse)
+    const [studentArray, updateArray] = useState(Array.from(currentCourse.students))
 
     const [formData, setFormData] = useState({
         Name: '',
@@ -18,42 +21,84 @@ const RosterComponent = () => {
     const OnChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value });
 
     const handleSubmit = async (e) => {
-        if(Name === '' || Email === '') alert("Please enter both name and email for the student!")
+        if(Name === '' || Email === '') {
+            alert("Please enter both name and email for the student!")
+        }
         else {
             e.preventDefault()
             const data = {
-                Name: Name,
-                Email: Email
+                email: Email,
+                abbreviation: currentCourse.abbreviation,
+                course_id: currentCourse.course_id,
+                course_name: currentCourse.course_name,
+                course_section: currentCourse.course_section,
+                semester: currentCourse.semester,
+                year: currentCourse.year
             };
+            updateArray((arr) => [...arr, Email])
             await axios.post(addStudentUrl, data)
+            setFalse()
         }
     }
+
+    const deleteStudent = async (Email) => {
+        const data = {
+            email: Email,
+            abbreviation: currentCourse.abbreviation,
+            course_id: currentCourse.course_id,
+            course_name: currentCourse.course_name,
+            course_section: currentCourse.course_section,
+            semester: currentCourse.semester,
+            year: currentCourse.year
+        };
+
+        let index = studentArray.indexOf(Email)
+        studentArray.splice(index, 1)
+        updateArray((arr) => [...arr])
+        await axios.post(deleteStudentUrl, data)
+    }
+
+    const addsStudent = () => {
+        return (
+            <div id="addStudentDiv">
+                <label>Name:</label>
+                <input type="text" className="rosterInput" name="Name" value={Name}
+                       required onChange={(e) => OnChange(e)}/>
+                <label>Email:</label>
+                <input type="text" className="rosterInput" name="Email" value={Email}
+                       required onChange={(e) => OnChange(e)}/>
+                <button id="addStudentButton" onClick={handleSubmit}>Add Student</button>
+            </div>
+        )
+    }
+
+    const [show, setShow] = useState(false)
+    const setTrue = () => setShow(true)
+    const setFalse = () => setShow(false)
+
     return (
         <div className="RosterPage">
             <div id="roster">
-                <h1>Roster</h1>
-                <table id="rosterTable">
+                <table className="rosterTable">
                     <tr>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Team</th>
-                        <th>Status</th>
+                        <th className="rosterHeader">Name</th>
+                        <th className="rosterHeader">Email</th>
+                        <th className="rosterHeader">Team</th>
+                        <th className="rosterHeader">Status</th>
                     </tr>
-                    <tr>
-                        <th>Joshua Smith</th>
-                        <th>jsmith60@oswego.edu</th>
-                        <th>loremIpsumTeam01</th>
-                        <th>Student</th>
-                    </tr>
+                    {studentArray.map(d =>
+                        <tr>
+                            <th>{d}</th>
+                            <th>{d.Email}</th>
+                            <th>{d.Team}</th>
+                            <th>{d.Status}</th>
+                            <span onClick={() => deleteStudent(d)} className="crossMark">&#10060;</span>
+                        </tr>
+                    )}
                 </table>
-                <div id="addStudentDiv">
-                    <label >Name:</label>
-                    <input className="rosterInput" type="text" value={Name} onChange={(e) => OnChange(e)}/>
-                    <label>Email:</label>
-                    <input className="rosterInput" type="text" value={Email} onChange={(e) => OnChange(e)}/>
-                    <button id="addStudentButton" onClick={handleSubmit}>Add Student</button>
-                </div>
             </div>
+            {show ? addsStudent(): <button className="button_plus" onClick={setTrue}>
+                <img className="button_plus" src={require("./styles/plus-purple.png")}/></button>}
         </div>
     )
 }
