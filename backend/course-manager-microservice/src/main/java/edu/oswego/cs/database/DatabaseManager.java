636b8d788c1480f7ec551fe9a1bea653f1829874
Logan Nguyen <Lgad31vn@gmail.com>
2022-03-27 13:31:44 -0400

    revert to production mode

--- backend/course-manager-microservice/src/main/java/edu/oswego/cs/database/DatabaseManager.java
+++ backend/course-manager-microservice/src/main/java/edu/oswego/cs/database/DatabaseManager.java
@@ -10,15 +10,15 @@ import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class DatabaseManager {
-    int studentPort = 27037;//Integer.parseInt(System.getenv("MONGO_PORT"));
-    // int professorPort = Integer.parseInt(System.getenv("MONGO2_PORT"));
-    int coursePort = 27039; //Integer.parseInt(System.getenv("MONGO3_PORT"));
-    // int assignmentPort = Integer.parseInt(System.getenv("MONGO4_PORT"));
-    // int teamPort = Integer.parseInt(System.getenv("MONGO5_PORT"));
-    String hostname = "moxie.cs.oswego.edu"; // System.getenv("MONGO_HOSTNAME");
-    String mongoDatabase = "cpr"; //System.getenv("MONGO_DATABASE");
-    String mongoUser = "csc480"; //System.getenv("MONGO_USERNAME");
-    String mongoPassword = "BaI2dktnzj1di6DRWcgobX37yPYZ7rmg"; //System.getenv("MONGO_PASSWORD");
+    int studentPort = Integer.parseInt(System.getenv("MONGO_PORT"));
+    int professorPort = Integer.parseInt(System.getenv("MONGO2_PORT"));
+    int coursePort = Integer.parseInt(System.getenv("MONGO3_PORT"));
+    int assignmentPort = Integer.parseInt(System.getenv("MONGO4_PORT"));
+    int teamPort = Integer.parseInt(System.getenv("MONGO5_PORT"));
+    String hostname = System.getenv("MONGO_HOSTNAME");
+    String mongoDatabase = System.getenv("MONGO_DATABASE");
+    String mongoUser = System.getenv("MONGO_USERNAME");
+    String mongoPassword = System.getenv("MONGO_PASSWORD");
 
     public MongoDatabase getStudentDB() {
         MongoCredential credentials = MongoCredential.createCredential(mongoUser, mongoDatabase, mongoPassword.toCharArray());
@@ -30,15 +30,15 @@ public class DatabaseManager {
         return client.getDatabase(mongoDatabase);
     }
 
-    // public MongoDatabase getProfessorDB() {
-    //     MongoCredential credentials = MongoCredential.createCredential(mongoUser, mongoDatabase, mongoPassword.toCharArray());
-    //     MongoClient client = new MongoClient(
-    //             new ServerAddress(hostname, professorPort),
-    //             credentials,
-    //             new MongoClientOptions.Builder().build()
-    //     );
-    //     return client.getDatabase(mongoDatabase);
-    // }
+    public MongoDatabase getProfessorDB() {
+        MongoCredential credentials = MongoCredential.createCredential(mongoUser, mongoDatabase, mongoPassword.toCharArray());
+        MongoClient client = new MongoClient(
+                new ServerAddress(hostname, professorPort),
+                credentials,
+                new MongoClientOptions.Builder().build()
+        );
+        return client.getDatabase(mongoDatabase);
+    }
 
     public MongoDatabase getCourseDB() {
         MongoCredential credentials = MongoCredential.createCredential(mongoUser, mongoDatabase, mongoPassword.toCharArray());
@@ -50,25 +50,25 @@ public class DatabaseManager {
         return client.getDatabase(mongoDatabase);
     }
 
-    // public MongoDatabase getAssignmentDB() {
-    //     MongoCredential credentials = MongoCredential.createCredential(mongoUser, mongoDatabase, mongoPassword.toCharArray());
-    //     MongoClient client = new MongoClient(
-    //             new ServerAddress(hostname, assignmentPort),
-    //             credentials,
-    //             new MongoClientOptions.Builder().build()
-    //     );
-    //     return client.getDatabase(mongoDatabase);
-    // }
+    public MongoDatabase getAssignmentDB() {
+        MongoCredential credentials = MongoCredential.createCredential(mongoUser, mongoDatabase, mongoPassword.toCharArray());
+        MongoClient client = new MongoClient(
+                new ServerAddress(hostname, assignmentPort),
+                credentials,
+                new MongoClientOptions.Builder().build()
+        );
+        return client.getDatabase(mongoDatabase);
+    }
 
-    // public MongoDatabase getTeamDB() {
-    //     MongoCredential credentials = MongoCredential.createCredential(mongoUser, mongoDatabase, mongoPassword.toCharArray());
-    //     MongoClient client = new MongoClient(
-    //             new ServerAddress(hostname, teamPort),
-    //             credentials,
-    //             new MongoClientOptions.Builder().build()
-    //     );
-    //     return client.getDatabase(mongoDatabase);
-    // }
+    public MongoDatabase getTeamDB() {
+        MongoCredential credentials = MongoCredential.createCredential(mongoUser, mongoDatabase, mongoPassword.toCharArray());
+        MongoClient client = new MongoClient(
+                new ServerAddress(hostname, teamPort),
+                credentials,
+                new MongoClientOptions.Builder().build()
+        );
+        return client.getDatabase(mongoDatabase);
+    }
 
     public DatabaseManager() {
     }
