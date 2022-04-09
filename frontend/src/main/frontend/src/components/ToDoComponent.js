2672f5885eca261176403a5a6ea7a48f0777a04b
jannyr08 <pokada816@gmail.com>
2022-04-09 16:31:29 -0400

    added ability to download files from student AssignmentPage

--- frontend/src/main/frontend/src/components/ToDoComponent.js
+++ frontend/src/main/frontend/src/components/ToDoComponent.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import {useDispatch, useSelector} from "react-redux";
 import "./styles/StudentAss.css"
 import {Link, useParams} from "react-router-dom";
-import { getAssignmentDetailsAsync, getAssignmentFilesAsync, getCourseAssignmentsAsync } from "../redux/features/assignmentSlice";
+import { getCourseAssignmentsAsync } from "../redux/features/assignmentSlice";
 
 const ToDoComponent = () => {
     const dispatch = useDispatch()
@@ -15,18 +15,11 @@ const ToDoComponent = () => {
         dispatch(getCourseAssignmentsAsync(courseId))
     },[])
 
-    const assignmentClickHandler = (assignment) => {
-        const assignmentId = assignment.assignment_id
-        dispatch(getAssignmentDetailsAsync({ courseId, assignmentId }));
-        dispatch(getAssignmentFilesAsync({ courseId, assignmentId }));
-    }
-
     return (
         <h3>
             <div id="assList">
                 {courseAssignments.map(assignment =>
-                    <Link to={`/details/${role}/${courseId}/${assignment.assignment_id}`}
-                          onClick={() => assignmentClickHandler(assignment)}>
+                    <Link to={`/details/${role}/${courseId}/${assignment.assignment_id}`}>
                         <li id="assListItem">{assignment.assignment_name + "\n\n" + "Due Date: " + assignment.due_date}</li>
                     </Link>
                 )}
