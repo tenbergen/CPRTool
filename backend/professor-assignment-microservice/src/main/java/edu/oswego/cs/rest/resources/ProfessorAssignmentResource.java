31d79437588efe4d0927fc7e26a98fde38072286
Growing-Pains <davisnicholasbusiness@gmail.com>
2022-04-10 01:36:40 -0400

    upload/remove/download file for pr, edited fileDAO & assignmnetDAO

--- backend/professor-assignment-microservice/src/main/java/edu/oswego/cs/rest/resources/ProfessorAssignmentResource.java
+++ backend/professor-assignment-microservice/src/main/java/edu/oswego/cs/rest/resources/ProfessorAssignmentResource.java
@@ -67,11 +67,44 @@ public class ProfessorAssignmentResource {
         return Response.status(Response.Status.OK).entity("Successfully added file to assignment.").build();
     }
 
+    /**
+     * File is uploaded as form-data and passed back as a List<IAttachment>
+     * The attachment is processed in FileDao.FileFactory, which reads and
+     * reconstructs the file through inputStream and outputStream respectively
+     *
+     * @param attachments  type List<IAttachment>: file(s) passed back as form-data
+     * @param courseID     type String
+     * @param assignmentID type int
+     * @return Response
+     */
+    @POST
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces({MediaType.MULTIPART_FORM_DATA, "application/pdf"})
+    @Path("/courses/{courseID}/assignments/{assignmentID}/peer-review/upload")
+    public Response addFileToPeerReview(List<IAttachment> attachments, @PathParam("courseID") String courseID, @PathParam("assignmentID") int assignmentID) throws Exception {
+        for (IAttachment attachment : attachments) {
+            if (attachment == null) continue;
+            String fileName = attachment.getDataHandler().getName();
+
+            if (!fileName.endsWith("pdf") && !fileName.endsWith("zip")) return Response.status(Response.Status.UNSUPPORTED_MEDIA_TYPE).build();
+            AssignmentInterface.writeToPeerReviews(FileDAO.fileFactory(fileName, courseID, attachment, assignmentID));
+        }
+        return Response.status(Response.Status.OK).entity("Successfully added file to peer reviews.").build();
+    }
+
     @DELETE
     @Consumes(MediaType.APPLICATION_JSON)
     @Path("/courses/assignments/remove-file")
     public Response removeFileFromAssignment(FileDAO fileDAO) {
         AssignmentInterface.removeFile(fileDAO);
+        return Response.status(Response.Status.OK).entity("File successfully deleted.").build();
+    }
+
+    @DELETE
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/courses/assignments/peer-review/remove-file")
+    public Response removeFileFromPeerReview(FileDAO fileDAO) {
+        AssignmentInterface.removePeerReviewFile(fileDAO);
         return Response.status(Response.Status.OK).entity("File successfully deleted.").build();
     }
 
@@ -135,6 +168,23 @@ public class ProfessorAssignmentResource {
         Response.ResponseBuilder response = Response.ok(file);
         response.header("Content-Disposition", "attachment; filename=" + file.getName());
         return response.build();
+    }
+
+    /**
+     * Retrieves the assignment from its location on the server and passes it to the front end via the request header
+     * as a stream. The request entity passes an InputStream[] with the assignment files in each array.
+     *
+     * @param courseID     String
+     * @param assignmentID int
+     * @return response
+     **/
+    @GET
+    @Produces(MediaType.MULTIPART_FORM_DATA)
+    @Path("/courses/{courseID}/assignments/{assignmentID}/peer-review/download/{fileName}")
+    public Response downloadPeerReview(@PathParam("courseID") String courseID, @PathParam("assignmentID") int assignmentID, @PathParam("fileName") String fileName) {
+        File file = new File(AssignmentInterface.findPeerReviewFile(courseID, assignmentID, fileName));
+
+        return Response.ok(file).header("Content-Disposition", "attachment; filename=" + file.getName()).build();
     }
 
     @DELETE
