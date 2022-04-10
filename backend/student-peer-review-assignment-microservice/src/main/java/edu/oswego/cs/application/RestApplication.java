7fdd5c5de41d35e413d6c9fe51292b919965a0a5
Growing-Pains <davisnicholasbusiness@gmail.com>
2022-04-10 12:06:24 -0400

    added view pr files

--- backend/student-peer-review-assignment-microservice/src/main/java/edu/oswego/cs/application/RestApplication.java
+++
@@ -1,9 +0,0 @@
-package edu.oswego.cs.application;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("peer-review")
-public class RestApplication extends Application {
-
-}
