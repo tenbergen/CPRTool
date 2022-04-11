9456ce7a96190feefe0f9d71154d2658a6bae0f1
gmaldona <gmaldona@oswego.edu>
2022-04-10 21:08:54 -0400

    project structure rearrange

--- backend/student-peer-review-assignment-microservice/src/main/java/edu/oswego/cs/resources/PeerReviewAssignmentResource.java
+++ backend/student-peer-review-assignment-microservice/src/main/java/edu/oswego/cs/resources/PeerReviewAssignmentResource.java
@@ -1,14 +1,8 @@
 package edu.oswego.cs.resources;
 
-import edu.oswego.cs.distribution.AssignmentDistribution;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
 
 @Path("assignments")
 public class PeerReviewAssignmentResource {
