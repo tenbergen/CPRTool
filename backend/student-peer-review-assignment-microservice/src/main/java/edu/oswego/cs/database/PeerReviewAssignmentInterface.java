6f11b3f8c70b4222929ddbcb058865f56d3cc6ea
Pierce Hurd <phurd@oswego.edu>
2022-04-12 11:27:00 -0400

    added file structure

--- backend/student-peer-review-assignment-microservice/src/main/java/edu/oswego/cs/database/PeerReviewAssignmentInterface.java
+++ backend/student-peer-review-assignment-microservice/src/main/java/edu/oswego/cs/database/PeerReviewAssignmentInterface.java
@@ -15,6 +15,7 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.File;
 import java.util.*;
 
 import static com.mongodb.client.model.Filters.eq;
@@ -26,6 +27,7 @@ public class PeerReviewAssignmentInterface {
     private final MongoCollection<Document> courseCollection;
     private final MongoCollection<Document> teamCollection;
     private final MongoCollection<Document> assignmentCollection;
+    private final String reg = "/";
 
     public PeerReviewAssignmentInterface() {
         DatabaseManager databaseManager = new DatabaseManager();
@@ -61,12 +63,33 @@ public class PeerReviewAssignmentInterface {
                 for (String team : peerReviewAssignments.keySet()) {
                     doc.put(team, peerReviewAssignments.get(team));
                 }
-
+                makeFileStructure(peerReviewAssignments.keySet(),courseID,assignmentID);
                 assignmentCollection.updateOne(assignmentDocument, set("assigned_teams", doc));
                 return doc;
             }
         }
         throw new WebApplicationException(Response.status(Response.Status.BAD_REQUEST).entity("Failed to add assigned teams.").build());
+    }
+    public boolean makeFileStructure(Set<String> teams,String courseID,int assignmentID){
+        String path = System.getProperty("user.dir")+ reg + courseID + reg + assignmentID;
+        ArrayList<String>validNames = new ArrayList<>();
+        validNames.add("peer-reviews");
+        validNames.add("assignments");
+        validNames.add("team-submissions");
+        validNames.add("peer-review-submissions");
+        File[] structure = new File(path).listFiles();
+        for(File f : structure){
+            if(validNames.contains(f.getName())){
+                validNames.remove(f.getName());
+            }else return false;
+        }
+        if(validNames.size()!=0)return false;
+        if(Objects.requireNonNull(new File(path + reg + "peer-review-submissions").listFiles()).length!=0)return false;
+        path += reg + "peer-review-submissions";
+        for(String team: teams){
+          new File(path+reg+team).mkdir();
+        }
+        return true;
     }
 
     /**
