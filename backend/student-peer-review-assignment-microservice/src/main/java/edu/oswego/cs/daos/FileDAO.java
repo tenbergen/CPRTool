06e9d83e3674270d53f1bd125b72e4ec70e74e58
gmaldona <gmaldona@oswego.edu>
2022-04-14 14:38:48 -0400

    Deleted zipping feature from fileDao

--- backend/student-peer-review-assignment-microservice/src/main/java/edu/oswego/cs/daos/FileDAO.java
+++ backend/student-peer-review-assignment-microservice/src/main/java/edu/oswego/cs/daos/FileDAO.java
@@ -31,40 +31,12 @@ public class FileDAO {
         this.inputStream = inputStream;
     }
 
-    public static void zipPeerReview(Map<String, List<String>> teamAssignments, String courseID, int assignmentID) throws IOException  {
-        List<File> files = Arrays.asList(new File(assignment_path + courseID + "/" + assignmentID).listFiles());
-        List<String> fileNames = files.stream().map(File::getName).collect(Collectors.toList());
-
-        teamAssignments.keySet().forEach( (teamName) -> {
-            String path = "peer-reviews/" + courseID+"/"+assignmentID+"/";
-            if (! new File(path).exists()) {
-                new File(path).mkdirs();
-            }
-            String zipfilename = path+ "for-"+teamName+".zip";
-
-            try {
-                FileOutputStream fileOutputStream = new FileOutputStream(zipfilename);
-                ZipOutputStream zipOutputStream  = new ZipOutputStream(fileOutputStream);
-                for (String assigned : teamAssignments.get(teamName)) {
-
-                    String filename = fileNames.stream().filter(fn -> fn.contains(assigned)).findFirst().get();
-                    zipOutputStream.putNextEntry(new ZipEntry("for-" + teamName+ "/"+filename));
-                    byte[] fileBytes = Files.readAllBytes(Paths.get(assignment_path+courseID+"/"+assignmentID+"/"+filename));
-                    zipOutputStream.write(fileBytes, 0, fileBytes.length);
-                    zipOutputStream.closeEntry();
-                }
-                zipOutputStream.close();
-            } catch (IOException e) { e.printStackTrace(); }
-        });
-
-    }
-
     public static FileDAO fileFactory(
             String courseID,
             String srcTeamName, String destTeamName,
             int assignmentID, IAttachment attachment) throws IOException {
         InputStream inputStream = attachment.getDataHandler().getInputStream();
-        String fileName = "peer-review-from-" + srcTeamName + "-to-" + destTeamName;
+        String fileName = "from-" + srcTeamName + "-to-" + destTeamName;
         return new FileDAO(fileName, courseID, srcTeamName, destTeamName, assignmentID, inputStream);
     }
 
