dd456d55822540ca7c42bfbd57fab008ad951c74
gmaldona <gmaldona@oswego.edu>
2022-04-14 14:41:52 -0400

    Changed endpoint upload and download to per team instead of zipping.

--- backend/student-peer-review-assignment-microservice/src/main/java/edu/oswego/cs/database/PeerReviewAssignmentInterface.java
+++ backend/student-peer-review-assignment-microservice/src/main/java/edu/oswego/cs/database/PeerReviewAssignmentInterface.java
@@ -1,35 +1,20 @@
 package edu.oswego.cs.database;
 
-import com.mongodb.client.FindIterable;
 import com.ibm.websphere.jaxrs20.multipart.IAttachment;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.Filters;
 import edu.oswego.cs.daos.FileDAO;
-import org.bson.BsonDocument;
 import org.bson.Document;
-import org.bson.conversions.Bson;
 
-import javax.json.bind.Jsonb;
-import javax.json.bind.JsonbBuilder;
 import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
 
 import static com.mongodb.client.model.Filters.eq;
-import static com.mongodb.client.model.Updates.push;
 import static com.mongodb.client.model.Updates.set;
 
 public class PeerReviewAssignmentInterface {
@@ -117,43 +102,28 @@ public class PeerReviewAssignmentInterface {
     }
 
     public void uploadPeerReview(String courseID, int assignmentID, String srcTeamName, String destTeamName, IAttachment attachment) throws IOException {
-        String basePath = FileDAO.peer_review_submission_path+courseID+"/"+assignmentID+"/";
-        if (! new File(basePath).exists()) {
-            new File(basePath).mkdirs();
-        }
-
         FileDAO fileDAO = FileDAO.fileFactory(courseID, srcTeamName, destTeamName, assignmentID, attachment);
-
-        OutputStream outputStream = new FileOutputStream(basePath+fileDAO.fileName+".pdf");
+        String path = "courses/"+courseID+"/"+assignmentID+"/peer-review-submissions/";
+        if (! new File(path).exists()) {
+            new File(path).mkdirs();
+        }
+        OutputStream outputStream = new FileOutputStream(path+fileDAO.fileName+".pdf");
         outputStream.write(fileDAO.inputStream.readAllBytes());
         outputStream.close();
 
     }
 
-    public String packagePeerReviewedAssignments(String courseID, int assignmentID, String teamName) {
-        if (!new File(FileDAO.peer_review_submission_path).exists())
+    public File downloadFinishedPeerReview(String courseID, int assignmentID, String srcTeamName, String destTeamName) {
+        String path = "courses/"+courseID+"/"+assignmentID+"/peer-review-submissions/";
+        if (!new File(path).exists())
             throw new WebApplicationException("Peer reviews do not exist for this course yet.");
 
-        String dir = FileDAO.peer_review_submission_path+courseID+"/"+assignmentID+"/";
-        List<File> files = Arrays.asList(new File(dir).listFiles());
+        Optional<File> file = Arrays.stream(new File(path).listFiles())
+                .filter( f -> f.getName().contains(srcTeamName) && f.getName().contains(destTeamName) )
+                .findFirst();
 
-        List<File> teamPeerReviews = files.stream()
-                .filter(f -> f.getName().split(".pdf")[0].endsWith(teamName))
-                .collect(Collectors.toList());
+        if (file.isEmpty()) throw new WebApplicationException("No peer review from team " + srcTeamName + " for " + destTeamName);
+        return file.get();
 
-        String zipPath = "peer-review-submissions/" + courseID+"/"+assignmentID+"/"+"for-"+teamName.concat(".zip");
-        try {
-            FileOutputStream fileOutputStream = new FileOutputStream(zipPath);
-            ZipOutputStream zipOutputStream = new ZipOutputStream(fileOutputStream);
-            for (File f : teamPeerReviews) {
-               zipOutputStream.putNextEntry(new ZipEntry("for-"+teamName+"/"+f.getName()));
-               byte[] fileBytes = Files.readAllBytes(Paths.get(dir+f.getName()));
-               zipOutputStream.write(fileBytes, 0, fileBytes.length);
-               zipOutputStream.closeEntry();
-            }
-            zipOutputStream.close();
-            return zipPath;
-        } catch (Exception e) {e.printStackTrace();}
-        return "";
     }
 }
