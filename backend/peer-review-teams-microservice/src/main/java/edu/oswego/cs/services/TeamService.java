7c90874817ee60b2f0ec7c8eb9329669c511dc4b
Logan Nguyen <Lgad31vn@gmail.com>
2022-04-14 23:42:29 -0400

    Cleaned up TeamService

--- backend/peer-review-teams-microservice/src/main/java/edu/oswego/cs/services/TeamService.java
+++ backend/peer-review-teams-microservice/src/main/java/edu/oswego/cs/services/TeamService.java
@@ -1,14 +1,11 @@
 package edu.oswego.cs.services;
 
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoCursor;
 import edu.oswego.cs.database.TeamInterface;
 import edu.oswego.cs.requests.TeamParam;
 import org.bson.Document;
 
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -17,7 +14,6 @@ public class TeamService {
 
     public String generateTeamID(String courseID){
         List<Document> teamDocuments = new TeamInterface().getAllTeams(courseID);
-        if (teamDocuments == null || teamDocuments.size() == 0) throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND).entity("No teams found.").build());
         
         Set<String> teamIDs = new HashSet<>();
         for (Document teamDocument : teamDocuments) 
@@ -25,7 +21,6 @@ public class TeamService {
         for (int i = 0; i < teamIDs.size(); i++)
             if (!teamIDs.contains(String.valueOf(i))) 
                 return String.valueOf(i);
-        
         return String.valueOf(teamIDs.size());
     }
 
@@ -47,15 +42,5 @@ public class TeamService {
             }
         }
         throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND).entity("Student not found.").build());
-    }
-
-    public List<String> getTeamIDsInCourse(TeamParam request) {
-        List<Document> teams = new TeamInterface().getAllTeams(request.getCourseID());
-        List<String> teamIDs = new ArrayList<>();
-        for (Document team : teams) {
-            String teamID = team.getString("team_id");
-            teamIDs.add(teamID);
-        }
-        return teamIDs;
     }
 }
