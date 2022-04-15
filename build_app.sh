da575ff5d38c84dbdce9c3ab7a0e95ef3ae234a6
Mint <developer@chocomint.dev>
2022-04-15 13:54:45 -0400

    Updated build script

--- build_app.sh
+++ build_app.sh
@@ -5,8 +5,10 @@ docker container stop cpr-frontend && docker container rm cpr-frontend
 docker container stop cpr-login && docker container rm cpr-login
 docker container stop cpr-course-manager && docker container rm cpr-course-manager
 docker container stop cpr-course-viewer && docker container rm cpr-course-viewer
-docker container stop cpr-peer-review-teams && docker container rm cpr-peer-review-teams
 docker container stop cpr-professor-assignment && docker container rm cpr-professor-assignment
+docker container stop cpr-peer-review-teams && docker container rm cpr-peer-review-teams
+docker container stop cpr-student-assignment && docker container rm cpr-student-assignment
+docker container stop cpr-student-peer-review-assignment && docker container rm cpr-student-peer-review-assignment
 docker container stop cpr-nginx && docker container rm cpr-nginx
 
 # cd into the working directory.
