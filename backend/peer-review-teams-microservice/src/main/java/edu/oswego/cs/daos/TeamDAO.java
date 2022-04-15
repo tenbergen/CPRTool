bc2919db17b2b624118ad96375a6d15a4dc1c85b
Mint <developer@chocomint.dev>
2022-04-15 16:28:31 -0400

    Updated DAO keys

--- backend/peer-review-teams-microservice/src/main/java/edu/oswego/cs/daos/TeamDAO.java
+++ backend/peer-review-teams-microservice/src/main/java/edu/oswego/cs/daos/TeamDAO.java
@@ -1,7 +1,6 @@
 package edu.oswego.cs.daos;
 
 import com.mongodb.lang.NonNull;
-
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -19,27 +18,26 @@ import java.util.List;
 public class TeamDAO {
     @Id @JsonbProperty("team_id") private String teamID;
     @JsonbProperty("course_id") private String courseID;
-    @JsonbProperty("is_finalized") @ElementCollection private List<String> isFinalized;
-    @JsonbProperty("is_full") private Boolean isFull;
+    @JsonbProperty("team_full") private Boolean teamFull;
     @JsonbProperty("team_lead") private String teamLead;
     @JsonbProperty("team_lock") private boolean teamLock;
     @JsonbProperty("team_members") @ElementCollection private List<String> teamMembers;
+    @JsonbProperty("team_name_votes") @ElementCollection private List<String> teamNameVotes;
     @JsonbProperty("team_size") private Integer teamSize;
 
     @JsonbCreator
-    public TeamDAO( 
-        @NonNull @JsonbProperty("team_id") String teamID,
-        @NonNull @JsonbProperty("course_id") String courseID,
-        @NonNull @JsonbProperty("team_size") Integer teamSize,
-        @NonNull @JsonbProperty("team_lead") String teamLead
-        ) {
+    public TeamDAO(
+            @NonNull @JsonbProperty("team_id") String teamID,
+            @NonNull @JsonbProperty("course_id") String courseID,
+            @NonNull @JsonbProperty("team_size") Integer teamSize,
+            @NonNull @JsonbProperty("team_lead") String teamLead) {
         this.teamID = teamID;
         this.courseID = courseID;
-        this.isFinalized = new ArrayList<>();
-        this.isFull = false;
+        this.teamFull = false;
         this.teamLead = teamLead;
         this.teamLock = false;
         this.teamMembers = new ArrayList<>();
+        this.teamNameVotes = new ArrayList<>();
         this.teamSize = teamSize;
     }
 }
