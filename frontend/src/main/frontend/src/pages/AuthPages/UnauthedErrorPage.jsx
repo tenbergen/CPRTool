2a2f0b504827fe05626710f5f3678a2fae28fb59
Logan Nguyen <Lgad31vn@gmail.com>
2022-04-18 17:30:03 -0400

    Updated UnauthPage's layout

--- frontend/src/main/frontend/src/pages/AuthPages/UnauthedErrorPage.jsx
+++ frontend/src/main/frontend/src/pages/AuthPages/UnauthedErrorPage.jsx
@@ -4,15 +4,22 @@ const logoutURL = `${window.location.protocol}//${window.location.host}/logout`;
 const UnauthedErrorPage = () => {
   return (
     <div id='unauth'>
-      <div id='unauthDiv'>
-        <div className='unauthContent'>
-          401 Not Authenticated
-          <span id='emoji'>&#128542;</span>
-        </div>
-        <div className='unauthContent'>
-          Please log in using your @oswego.edu account!
-          <br />
-          <a href={logoutURL}>Go Back</a>
+      <div className='unauthContent'>
+        <div className='unauth-wrapper'>
+          <div className='unauth-stm'>
+            Not Authenticated!
+            <span id='emoji'> &#128542;</span>
+          </div>
+          <div className='unauth-instruction'>
+            Please log in using your{' '}
+            <span class='auth-oswego'> @oswego.edu </span> account!
+            <br />
+            <div className='go-back-wrapper'>
+              <a className='go-back-link' href={logoutURL}>
+                Go Back
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
