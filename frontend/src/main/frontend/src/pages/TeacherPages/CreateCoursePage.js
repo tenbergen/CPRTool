39169a506221c2c7d395d8edde3f857e234cc780
Logan Nguyen <Lgad31vn@gmail.com>
2022-04-17 23:41:03 -0400

    Added loader to createCoursePage

--- frontend/src/main/frontend/src/pages/TeacherPages/CreateCoursePage.js
+++ frontend/src/main/frontend/src/pages/TeacherPages/CreateCoursePage.js
@@ -1,12 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './styles/CreateCourseStyle.css';
 import SidebarComponent from '../../components/SidebarComponent';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import Loader from '../../components/LoaderComponenets/Loader';
 
 const CreateCoursePage = () => {
   const submitCourseUrl = `${process.env.REACT_APP_URL}/manage/professor/courses/course/create`;
   let navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setTimeout(() => setIsLoading(false), 200);
+  }, []);
 
   const [formData, setFormData] = useState({
     course_name: '',
@@ -64,106 +71,112 @@ const CreateCoursePage = () => {
   };
 
   return (
-    <div className='cpp-parent'>
-      <SidebarComponent />
-      <div className='cpp-container'>
-        <h2 className='cpp-title'> Add new course </h2>
-        <form className='ccp-form'>
-          <div className='ccp-input-field'>
-            <label>
-              {' '}
-              <b> Course name: </b>{' '}
-            </label>
-            <input
-              type='text'
-              name='course_name'
-              value={course_name}
-              required
-              onChange={(e) => OnChange(e)}
-            />
+    <div>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className='cpp-parent'>
+          <SidebarComponent />
+          <div className='cpp-container'>
+            <h2 className='cpp-title'> Add new course </h2>
+            <form className='ccp-form'>
+              <div className='ccp-input-field'>
+                <label>
+                  {' '}
+                  <b> Course name: </b>{' '}
+                </label>
+                <input
+                  type='text'
+                  name='course_name'
+                  value={course_name}
+                  required
+                  onChange={(e) => OnChange(e)}
+                />
+              </div>
+
+              <div className='cpp-row-multiple'>
+                <div className='ccp-input-field'>
+                  <label>
+                    {' '}
+                    <b> Course abbreviation: </b>{' '}
+                  </label>
+                  <input
+                    type='text'
+                    name='abbreviation'
+                    value={abbreviation}
+                    required
+                    onChange={(e) => OnChange(e)}
+                  />
+                </div>
+
+                <div className='ccp-input-field'>
+                  <label>
+                    {' '}
+                    <b> Course section: </b>{' '}
+                  </label>
+                  <input
+                    type='text'
+                    name='course_section'
+                    value={course_section}
+                    required
+                    onChange={(e) => OnChange(e)}
+                  />
+                </div>
+              </div>
+
+              <div className='cpp-row-multiple'>
+                <div className='ccp-input-field'>
+                  <label>
+                    {' '}
+                    <b> Semester: </b>{' '}
+                  </label>
+                  <input
+                    type='text'
+                    name='semester'
+                    value={semester}
+                    required
+                    onChange={(e) => OnChange(e)}
+                  />
+                </div>
+
+                <div className='ccp-input-field'>
+                  <label>
+                    {' '}
+                    <b> Year: </b>{' '}
+                  </label>
+                  <input
+                    type='number'
+                    min={new Date().getFullYear().toString()}
+                    step='1'
+                    name='year'
+                    value={year}
+                    required
+                    onChange={(e) => OnChange(e)}
+                  />
+                </div>
+              </div>
+
+              <div className='ccp-input-field'>
+                <label>
+                  {' '}
+                  <b> CRN: </b>{' '}
+                </label>
+                <input
+                  type='number'
+                  name='crn'
+                  value={crn}
+                  required
+                  onChange={(e) => OnChange(e)}
+                />
+              </div>
+
+              <div className='ccp-button'>
+                <button onClick={handleSubmit}> Create </button>
+              </div>
+            </form>
           </div>
-
-          <div className='cpp-row-multiple'>
-            <div className='ccp-input-field'>
-              <label>
-                {' '}
-                <b> Course abbreviation: </b>{' '}
-              </label>
-              <input
-                type='text'
-                name='abbreviation'
-                value={abbreviation}
-                required
-                onChange={(e) => OnChange(e)}
-              />
-            </div>
-
-            <div className='ccp-input-field'>
-              <label>
-                {' '}
-                <b> Course section: </b>{' '}
-              </label>
-              <input
-                type='text'
-                name='course_section'
-                value={course_section}
-                required
-                onChange={(e) => OnChange(e)}
-              />
-            </div>
-          </div>
-
-          <div className='cpp-row-multiple'>
-            <div className='ccp-input-field'>
-              <label>
-                {' '}
-                <b> Semester: </b>{' '}
-              </label>
-              <input
-                type='text'
-                name='semester'
-                value={semester}
-                required
-                onChange={(e) => OnChange(e)}
-              />
-            </div>
-
-            <div className='ccp-input-field'>
-              <label>
-                {' '}
-                <b> Year: </b>{' '}
-              </label>
-              <input
-                type='number'
-                min={new Date().getFullYear().toString()}
-                step='1'
-                name='year'
-                value={year}
-                required
-                onChange={(e) => OnChange(e)}
-              />
-            </div>
-          </div>
-
-          <div className='ccp-input-field'>
-            <label>
-              {' '}
-              <b> CRN: </b>{' '}
-            </label>
-            <input
-              type='number'
-              name='crn'
-              value={crn}
-              required
-              onChange={(e) => OnChange(e)}
-            />
-          </div>
-
-          <div className='ccp-button'>
-            <button onClick={handleSubmit}> Create </button>
-          </div>
-        </form>
-      </div>
+        </div>
+      )}
     </div>
   );
 };
