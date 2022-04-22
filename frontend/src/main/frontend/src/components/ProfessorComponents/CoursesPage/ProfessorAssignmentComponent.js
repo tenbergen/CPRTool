f823b8cc8836fe575e1f395290e72aad4ac3c2bd
Logan Nguyen <Lgad31vn@gmail.com>
2022-04-22 14:42:18 -0400

    Fixed Assignment delete-btn layout

--- frontend/src/main/frontend/src/components/ProfessorComponents/CoursesPage/ProfessorAssignmentComponent.js
+++ frontend/src/main/frontend/src/components/ProfessorComponents/CoursesPage/ProfessorAssignmentComponent.js
@@ -1,112 +1,115 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../styles/TeacherAss.css';
-import {useDispatch, useSelector} from 'react-redux';
-import {Link, useParams} from 'react-router-dom';
-import {getCourseAssignmentsAsync} from '../../../redux/features/assignmentSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useParams } from 'react-router-dom';
+import { getCourseAssignmentsAsync } from '../../../redux/features/assignmentSlice';
 import axios from 'axios';
 import Loader from '../../LoaderComponenets/Loader';
 
 const assignmentUrl = `${process.env.REACT_APP_URL}/assignments/professor/courses`;
 
 const ProfessorAssignmentComponent = () => {
-    const [isLoading, setIsLoading] = useState(false);
-    const dispatch = useDispatch();
-    const {courseId} = useParams();
-    const {courseAssignments, currentAssignmentLoaded} = useSelector(
-        (state) => state.assignments
+  const [isLoading, setIsLoading] = useState(false);
+  const dispatch = useDispatch();
+  const { courseId } = useParams();
+  const { courseAssignments, currentAssignmentLoaded } = useSelector(
+    (state) => state.assignments
+  );
+  const ass = 'Assignment';
+  const peer = 'Peer Review';
+
+  useEffect(() => {
+    setIsLoading(true);
+    dispatch(getCourseAssignmentsAsync(courseId));
+    setIsLoading(false);
+  }, []);
+
+  const confirmDelete = (assignment) => {
+    let confirmAction = window.confirm(
+      'Are you sure to delete this assignment?'
     );
-    const ass = "Assignment"
-    const peer = "Peer Review"
+    if (confirmAction) {
+      deleteAssignment(assignment);
+    }
+  };
 
-    useEffect(() => {
-        setIsLoading(true);
-        dispatch(getCourseAssignmentsAsync(courseId));
-        setIsLoading(false);
-    }, []);
+  console.log(courseAssignments);
 
-    const confirmDelete = (assignment) => {
-        let confirmAction = window.confirm(
-            'Are you sure to delete this assignment?'
-        );
-        if (confirmAction) {
-            deleteAssignment(assignment);
-        }
-    };
+  const deleteAssignment = async (assignment) => {
+    setIsLoading(true);
+    const url = `${assignmentUrl}/${courseId}/assignments/${assignment.assignment_id}/remove`;
+    await axios.delete(url).then((res) => {
+      console.log(res);
+    });
+    dispatch(getCourseAssignmentsAsync(courseId));
+    if (currentAssignmentLoaded) alert('Assignment successfully deleted.');
+    setIsLoading(false);
+  };
 
-    console.log(courseAssignments);
-
-    const deleteAssignment = async (assignment) => {
-        setIsLoading(true);
-        const url = `${assignmentUrl}/${courseId}/assignments/${assignment.assignment_id}/remove`;
-        await axios.delete(url).then((res) => {
-            console.log(res);
-        });
-        dispatch(getCourseAssignmentsAsync(courseId));
-        if (currentAssignmentLoaded) alert('Assignment successfully deleted.');
-        setIsLoading(false);
-    };
-
-    return (
-        <div>
-            {isLoading ? (
-                <div className='loader-wrapper'>
-                    <Loader/>
-                </div>
-            ) : (
-                <div className={'TeacherAss'}>
-                    <div id='ass'>
-                        <div id='teacherAssList'>
-                            {courseAssignments.map((assignment) => (
-                                <div className='assListItem'>
-                                    <Link
-                                        to={`/details/professor/${courseId}/${assignment.assignment_id}/grade`}
-                                    >
-                                        <li>
-                                            <span className='ass-span'>
-                                            {
-                                                assignment.assignment_type === 'peer-review'
-                                                    ? peer
-                                                    : ass
-                                            }
-                                            </span>
-                                            <br/>
-                                            <div className='ass-title'>
-                                                {assignment.assignment_name}
-                                                <span1>Due Date: {
-                                                    assignment.assignment_type === "peer-review"
-                                                        ? assignment.peer_review_due_date
-                                                        : assignment.due_date
-                                                }
-                                                </span1>
-                                                <br/>
-                                            </div>
-                                        </li>
-                                    </Link>
-                                    <div>
-                                        <p>
-                                            <div className='delete-btn-wrapper'>
-                                                <div
-                                                    className='delete-ass-btn'
-                                                    onClick={() => confirmDelete(assignment)}
-                                                >
-                                                    Delete Assignment
-                                                </div>
-                                            </div>
-                                        </p>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                        <div id='assAddClass'>
-                            <Link to='create/assignment'>
-                                <button className="blue-button-filled outfit-20" id='assAddButton'>Create new assignment</button>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-            )}
+  return (
+    <div>
+      {isLoading ? (
+        <div className='loader-wrapper'>
+          <Loader />
         </div>
-    );
+      ) : (
+        <div className={'TeacherAss'}>
+          <div id='ass'>
+            <div id='teacherAssList'>
+              {courseAssignments.map((assignment) => (
+                <div className='assListItem'>
+                  <Link
+                    to={`/details/professor/${courseId}/${assignment.assignment_id}/grade`}
+                  >
+                    <li>
+                      <span className='ass-span'>
+                        {assignment.assignment_type === 'peer-review'
+                          ? peer
+                          : ass}
+                      </span>
+                      <br />
+                      <div className='ass-title'>
+                        {assignment.assignment_name}
+                        <span1>
+                          Due Date:{' '}
+                          {assignment.assignment_type === 'peer-review'
+                            ? assignment.peer_review_due_date
+                            : assignment.due_date}
+                        </span1>
+                        <br />
+                      </div>
+                    </li>
+                  </Link>
+                  <div>
+                    <p>
+                      <div className='delete-btn-wrapper'>
+                        <div
+                          className='delete-ass-btn'
+                          onClick={() => confirmDelete(assignment)}
+                        >
+                          Delete Assignment
+                        </div>
+                      </div>
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div id='assAddClass'>
+              <Link to='create/assignment'>
+                <button
+                  className='blue-button-filled outfit-20'
+                  id='assAddButton'
+                >
+                  Create new assignment
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default ProfessorAssignmentComponent;
--- frontend/src/main/frontend/src/pages/AuthPages/LoginPage.js
+++ frontend/src/main/frontend/src/pages/AuthPages/LoginPage.js
@@ -7,53 +7,52 @@ import { useNavigate } from 'react-router-dom';
 import Loader from '../../components/LoaderComponenets/Loader';
 
 function LoginPage() {
-    const [isLoading, setIsLoading] = useState(false);
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const REACT_APP_CLIENT_ID = `${process.env.REACT_APP_CLIENT_ID}`;
+  const [isLoading, setIsLoading] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const REACT_APP_CLIENT_ID = `${process.env.REACT_APP_CLIENT_ID}`;
 
-    useEffect(() => {
-        setIsLoading(true);
-        setTimeout(() => setIsLoading(false), 200);
-    }, []);
+  useEffect(() => {
+    setIsLoading(true);
+    setTimeout(() => setIsLoading(false), 775);
+  }, []);
 
-    const handleFailure = (result) => {
-        console.log(result);
-        navigate('/unauthenticated');
-    };
+  const handleFailure = (result) => {
+    console.log(result);
+    navigate('/unauthenticated');
+  };
 
-    const handleLogin = async (googleData) => {
-        setIsLoading(true);
-        localStorage.setItem('google_token', googleData.tokenId);
-        dispatch(getTokenAsync());
-    };
+  const handleLogin = async (googleData) => {
+    localStorage.setItem('google_token', googleData.tokenId);
+    dispatch(getTokenAsync());
+  };
 
-    return (
-        <div>
-            {isLoading ? (
-                <Loader />
-            ) : (
-                <div className='bigBox'>
-                    <div id='box'>
-                        <div className='kumba-40 welcome'>Welcome!</div>
-                        <GoogleLogin
-                            className='googleButton'
-                            clientId={REACT_APP_CLIENT_ID}
-                            buttonText='Log in with Google'
-                            onSuccess={handleLogin}
-                            onFailure={handleFailure}
-                            hostedDomain={'oswego.edu'}
-                            cookiePolicy={'single_host_origin'}
-                            prompt='select_account'
-                        />
-                        <a href='' className='started'>
-                            Get started as an Instructor
-                        </a>
-                    </div>
-                </div>
-            )}
+  return (
+    <div>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className='bigBox'>
+          <div id='box'>
+            <div className='kumba-40 welcome'>Welcome!</div>
+            <GoogleLogin
+              className='googleButton'
+              clientId={REACT_APP_CLIENT_ID}
+              buttonText='Log in with Google'
+              onSuccess={handleLogin}
+              onFailure={handleFailure}
+              hostedDomain={'oswego.edu'}
+              cookiePolicy={'single_host_origin'}
+              prompt='select_account'
+            />
+            <a href='' className='started'>
+              Get started as an Instructor
+            </a>
+          </div>
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
 export default LoginPage;
