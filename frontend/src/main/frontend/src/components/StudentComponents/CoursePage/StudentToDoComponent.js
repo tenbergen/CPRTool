8e273150a718e5a0d1ff8c10ba5fbcb112385c4b
jannyr08 <pokada816@gmail.com>
2022-04-23 22:19:14 -0400

    fixed up assignment tiles

--- frontend/src/main/frontend/src/components/StudentComponents/CoursePage/StudentToDoComponent.js
+++ frontend/src/main/frontend/src/components/StudentComponents/CoursePage/StudentToDoComponent.js
@@ -1,36 +1,23 @@
 import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import '../../styles/StudentAss.css';
-import {Link, useParams} from 'react-router-dom';
+import {useNavigate, useParams} from 'react-router-dom';
 import {getCombinedAssignmentPeerReviews} from '../../../redux/features/assignmentSlice';
 import axios from "axios";
+import "./styles/StudentToDoStyle.css"
 
-const StudentToDoComponent = () => {
-    const dispatch = useDispatch();
+const AssignmentTile = ({assignment}) => {
+    const navigate = useNavigate()
+    const title =  assignment.assignment_type === 'peer-review' ? "Peer Review" : "Assignment"
+
     const store = useSelector((state) => state);
     const {role} = store.auth;
-    const {combinedAssignmentPeerReviews, assignmentsLoaded} =
-        store.assignments;
+
     const {courseId} = useParams();
-    const {currentTeamId, teamLoaded} = useSelector((state) => state.teams)
-    const ass = "Assignment"
-    const peer = "Peer Review"
-    const link = `/details/${role}/${courseId}`;
+    const link = `/details/${role}/${courseId}/${assignment.assignment_id}`;
 
-    useEffect(() => {
-        dispatch(getCombinedAssignmentPeerReviews({courseId, currentTeamId}));
-    }, []);
-
-    const onAssClick = async (assignment) => {
-        const fileName = assignment.assignment_instructions
-        const assignmentId = assignment.assignment_id
-        const url = `${process.env.REACT_APP_URL}/assignments/professor/courses/${courseId}/assignments/${assignmentId}/download/${fileName}`
-        await axios.get(url, {responseType: 'blob'})
-            .then(res => downloadFile(res.data, fileName))
-    }
-
-    const onPeerClick = async (assignment) => {
-        const fileName = assignment.peer_review_rubric
+    const onFileClick = async () => {
+        const fileName = assignment.assignment_type === "peer-review" ? assignment.peer_review_rubric : assignment.assignment_instructions
         const assignmentId = assignment.assignment_id
         const url = `${process.env.REACT_APP_URL}/assignments/professor/courses/${courseId}/assignments/${assignmentId}/download/${fileName}`
         await axios.get(url, {responseType: 'blob'})
@@ -45,50 +32,48 @@ const StudentToDoComponent = () => {
         href.click();
     }
 
+    const onTileClick = () => {
+        const tileLink =  assignment.assignment_type === 'peer-review'
+            ? `${link}/peer-review/${assignment.peer_review_team}`
+            : `${link}/normal`
+        navigate(tileLink)
+    }
+
+    return (
+        <div>
+            <div className="ass-tile">
+                <div className="outfit-16 ass-tile-title"> <span> {title} </span></div>
+                <div className="ass-tile-content">
+                   <div className="ass-tile-info" onClick={onTileClick}>
+                       <span className="kumba-27"> {assignment.assignment_name} </span>
+                       <span className="kumba-25"> {assignment.due_date} </span>
+                   </div>
+                    <span className="outfit-16 ass-tile-files" onClick={onFileClick}>
+                        {assignment.assignment_type === 'peer-review' ? assignment.peer_review_rubric : assignment.assignment_instructions}
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const StudentToDoComponent = () => {
+    const dispatch = useDispatch();
+    const store = useSelector((state) => state);
+    const {combinedAssignmentPeerReviews, assignmentsLoaded} = store.assignments;
+    const {courseId} = useParams();
+    const {currentTeamId, teamLoaded} = useSelector((state) => state.teams)
+
+    useEffect(() => {
+        dispatch(getCombinedAssignmentPeerReviews({courseId, currentTeamId}));
+    }, []);
+
     return (
         <h3>
             {assignmentsLoaded && teamLoaded ? (
                 <div id='assList'>
-                    {combinedAssignmentPeerReviews.map((assignment) => (
-                        <div className='assListItem'>
-                            <Link
-                                to={
-                                    assignment.assignment_type === 'peer-review'
-                                        ? `${link}/${assignment.assignment_id}/peer-review/${assignment.peer_review_team}`
-                                        : `${link}/${assignment.assignment_id}/normal`
-                                }
-                            >
-                                <li>
-                                    <span className='ass-span-student'>
-                                        {
-                                            assignment.assignment_type === 'peer-review'
-                                            ? peer
-                                            : ass
-                                        }
-                                    </span>
-                                    <br></br>
-                                    <div className='ass-title'>
-                                        {assignment.assignment_name}
-                                        <span className="span1-ap">Due Date: {
-                                            assignment.assignment_type === "peer-review"
-                                                ? assignment.peer_review_due_date
-                                                : assignment.due_date
-                                        }
-                                        </span>
-                                        <br></br>
-                                    </div>
-                                </li>
-                            </Link>
-                            {
-                                assignment.assignment_type === 'peer-review'
-                                    ? <div className='ass-instructions' onClick={() => onPeerClick(assignment)}>
-                                        {assignment.peer_review_rubric}
-                                    </div>
-                                    : <div className='ass-instructions' onClick={() => onAssClick(assignment)}>
-                                        {assignment.assignment_instructions}
-                                    </div>
-                            }
-                        </div>
+                    {combinedAssignmentPeerReviews.map(assignment => (
+                        <AssignmentTile assignment={assignment}/>
                     ))}
                 </div>
             ) : null}
