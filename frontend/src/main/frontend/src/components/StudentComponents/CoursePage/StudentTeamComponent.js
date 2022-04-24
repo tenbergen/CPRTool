5ff7312223cb6ed22cf48544c191df673d1e757d
jannyr08 <pokada816@gmail.com>
2022-04-23 20:36:19 -0400

    cleaned up team component logic

--- frontend/src/main/frontend/src/components/StudentComponents/CoursePage/StudentTeamComponent.js
+++ frontend/src/main/frontend/src/components/StudentComponents/CoursePage/StudentTeamComponent.js
@@ -1,66 +1,64 @@
 import React, {useEffect, useState} from 'react'
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import "../../styles/StudentTeamStyle.css"
-import {useNavigate, useParams} from "react-router-dom";
+import {useParams} from "react-router-dom";
 import axios from "axios";
+import {getCurrentCourseTeamAsync} from "../../../redux/features/teamSlice";
 
 const TeamComponent = () => {
-    const navigate = useNavigate()
+    const dispatch = useDispatch()
     const {courseId} = useParams()
-    const getTeamsUrl = `${process.env.REACT_APP_URL}/teams/team/get/all/` + courseId
-    const [teams, setTeams] = useState(Array())
+    const getTeamsUrl = `${process.env.REACT_APP_URL}/teams/team/get/all/${courseId}`
+    const [teams, setTeams] = useState([])
     const {lakerId} = useSelector((state) => state.auth)
     
     useEffect(async () => {
-        await axios.get(getTeamsUrl).then(r => {
-            for (let i = 0; i < r.data.length; i++) {
-                if(r.data[i].team_full === false) setTeams(arr => [...arr, r.data[i]])
-            }
-            console.log(r)
-        })
+        const allTeams = await axios.get(getTeamsUrl)
+            .then(res => {
+                console.log(res)
+                if (res.data.length > 0) return res.data
+                return []
+            })
+            .catch(e => {
+                console.log(e)
+                return []
+            })
+        const openTeams = allTeams.filter(team => !team.team_full)
+        setTeams(openTeams)
     }, [])
 
     const joinTeam = async (teamId) => {
         const joinUrl = `${process.env.REACT_APP_URL}/teams/team/join`
-        const data = JSON.parse(JSON.stringify({"team_id": teamId, "course_id": courseId, "student_id": lakerId}))
+        const data = {"team_id": teamId, "course_id": courseId, "student_id": lakerId}
         await axios.put(joinUrl, data)
-        .then(res => {
-            console.log(res)
-            navigate(`/`)
-            navigate(`/details/student/` + courseId)
-        }).catch((e) => {
-            console.log(e)
-            alert("Error joining team")
-        })
+            .then(res => {
+                console.log(res)
+                alert(`Successfully joined team ${teamId}`)
+                dispatch(getCurrentCourseTeamAsync({courseId, lakerId}))
+            }).catch((e) => {
+                console.log(e)
+                alert("Error joining team")
+            })
     }
 
     const createTeam = async () => {
         const team_name = prompt("Enter team name: ")
         const createUrl = `${process.env.REACT_APP_URL}/teams/team/create`
-        const createData = JSON.parse(JSON.stringify({"course_id": courseId, "student_id": lakerId, "team_name": team_name}))
-        if(team_name.split(" ").length > 1) {
+        const createData = {"course_id": courseId, "student_id": lakerId, "team_name": team_name}
+        if (team_name.split(" ").length > 1) {
             alert("Please enter a team name with no spaces!")
+            return
         }
-        else {
-            await axios.post(createUrl, createData)
-                .then(res => {
-                    console.log(res)
-                    navigate(`/`)
-                    navigate(`/details/student/` + courseId)
-                }).catch((e) => {
-                    console.log(e)
-                    alert("Error creating team")
-                })
-        }
-        // await axios.post(createUrl, createData)
-        // .then(res => {
-        //     console.log(res)
-        //     navigate(`/`)
-        //     navigate(`/details/student/` + courseId)
-        // }).catch((e) => {
-        //     console.log(e)
-        //     alert("Error creating team")
-        // })
+
+        await axios.post(createUrl, createData)
+            .then(res => {
+                console.log(res)
+                alert("Successfully created team")
+                dispatch(getCurrentCourseTeamAsync({courseId, lakerId}))
+            }).catch((e) => {
+                console.log(e)
+                alert("Error creating team")
+            })
     }
 
     return (
