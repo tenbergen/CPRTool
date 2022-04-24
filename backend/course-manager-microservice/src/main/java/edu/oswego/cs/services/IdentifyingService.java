a9b33d8154eef96716849973be860e314cd71c5e
Logan Nguyen <Lgad31vn@gmail.com>
2022-04-23 22:50:45 -0400

    Added IdentifyingService for courseManager

---
+++ backend/course-manager-microservice/src/main/java/edu/oswego/cs/services/IdentifyingService.java
@@ -0,0 +1,13 @@
+package edu.oswego.cs.services;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+
+public class IdentifyingService {
+    public void identifyingStudentService(SecurityContext securityContext, String professorID) {
+        String userID = securityContext.getUserPrincipal().getName().split("@")[0];
+        if (!professorID.equals(userID))
+            throw new WebApplicationException(Response.status(Response.Status.FORBIDDEN).entity("User principal name doesn't match.").build());
+    }
+}
