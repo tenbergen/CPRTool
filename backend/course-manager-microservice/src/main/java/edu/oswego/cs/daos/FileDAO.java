f4ef8b89ef2c5a30e83f0099aa431dc3d5ec35c6
Gregory Maldonado <gmaldona@oswego.edu>
2022-04-28 19:54:29 -0400

    fixed regex for csv parsing

--- backend/course-manager-microservice/src/main/java/edu/oswego/cs/daos/FileDAO.java
+++ backend/course-manager-microservice/src/main/java/edu/oswego/cs/daos/FileDAO.java
@@ -40,7 +40,7 @@ public class FileDAO {
                     reader.close();
 
                     csvLines = csvLines.stream()
-                            .map( str -> str.replaceAll("[^a-zA-Z0-9_-]", "") )
+                            .map( str -> str.replaceAll("[!@#$%^&*(){}|?<>:;]", "") )
                             .collect(Collectors.toList());
 
                     return new FileDAO(fileName, csvLines);
