3582400fe781877a9c98ee1f48a4d28931cfdc02
Logan Nguyen <nam.p.nguyen131@gmail.com>
2022-08-27 12:31:57 -0400

    Removed loader in Roster

--- frontend/src/main/frontend/src/components/ProfessorComponents/CoursesPage/ProfessorRosterComponent.js
+++ frontend/src/main/frontend/src/components/ProfessorComponents/CoursesPage/ProfessorRosterComponent.js
@@ -7,18 +7,15 @@ import {
   getCurrentCourseStudentsAsync,
 } from '../../../redux/features/courseSlice';
 import { useParams } from 'react-router-dom';
-import noStudent from '../../../assets/no_student.png';
+import noStudent from '../../../assets/no-student.png';
 import { CgAdd } from 'react-icons/cg';
-import LoaderComponent from '../../LoaderComponenets/LoaderComponent';
 import uuid from 'react-uuid';
 
 const ProfessorRosterComponent = () => {
   const dispatch = useDispatch();
   const { courseId } = useParams();
   const url = `${process.env.REACT_APP_URL}/manage/professor/courses`;
-  const { currentCourseStudents, currentCourseStudentsLoaded } = useSelector(
-    (state) => state.courses
-  );
+  const { currentCourseStudents } = useSelector((state) => state.courses);
 
   useEffect(() => {
     dispatch(getCurrentCourseStudentsAsync(courseId));
@@ -124,89 +121,83 @@ const ProfessorRosterComponent = () => {
     <div>
       <div className='RosterPage'>
         <div id='roster'>
-          {!currentCourseStudentsLoaded ? (
-            <LoaderComponent />
-          ) : (
-            <div className='roster-wrapper'>
-              {currentCourseStudents.length > 0 ? (
-                <div>
-                  <table className='rosterTable'>
-                    <thead>
-                      <tr>
-                        <th className='rosterHeader'>Name</th>
-                        <th className='rosterHeader'>Email</th>
-                        <th className='rosterHeader'>Team</th>
-                        <th className='rosterHeader'></th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {currentCourseStudents.map(
-                        (student) =>
-                          student && (
-                            <tr key={uuid()}>
-                              <th className='rosterComp'>
-                                {student.first_name
-                                  ? student.first_name + ' ' + student.last_name
-                                  : ''}
-                              </th>
-                              <th className='rosterComp'>
-                                {student.student_id}
-                              </th>
-                              <th className='rosterComp'>
-                                {student.team !== null ? student.team : ''}
-                              </th>
-                              <th className='rosterComp'>
-                                <div className='crossMark-wrapper'>
-                                  <div
-                                    onClick={() => deleteStudent(student)}
-                                    className='crossMark'
-                                  >
-                                    X
-                                  </div>
+          <div className='roster-wrapper'>
+            {currentCourseStudents.length > 0 ? (
+              <div>
+                <table className='rosterTable'>
+                  <thead>
+                    <tr>
+                      <th className='rosterHeader'>Name</th>
+                      <th className='rosterHeader'>Email</th>
+                      <th className='rosterHeader'>Team</th>
+                      <th className='rosterHeader'></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {currentCourseStudents.map(
+                      (student) =>
+                        student && (
+                          <tr key={uuid()}>
+                            <th className='rosterComp'>
+                              {student.first_name
+                                ? student.first_name + ' ' + student.last_name
+                                : ''}
+                            </th>
+                            <th className='rosterComp'>{student.student_id}</th>
+                            <th className='rosterComp'>
+                              {student.team !== null ? student.team : ''}
+                            </th>
+                            <th className='rosterComp'>
+                              <div className='crossMark-wrapper'>
+                                <div
+                                  onClick={() => deleteStudent(student)}
+                                  className='crossMark'
+                                >
+                                  X
                                 </div>
-                              </th>
-                            </tr>
-                          )
-                      )}
-                    </tbody>
-                  </table>
-                </div>
-              ) : (
-                <div className='no-student-container'>
-                  <div className='no-student-wrapper'>
-                    <div className='no-student-img-wrapper'>
-                      <img
-                        className='no-student-img'
-                        src={noStudent}
-                        alt='no_students'
-                        onClick={() => {
-                          setTrue();
-                        }}
-                      />
-                    </div>
-                    <div className='no-student-header'>No Students Added</div>
-                    <div className='no-student-description'>
-                      Hit the plus button to add student
-                    </div>
-                  </div>
-                </div>
-              )}
-              {show ? (
-                addStudent()
-              ) : (
-                <div className='plus-button-container'>
-                  <div className='plus-button-wrapper'>
-                    <CgAdd
-                      onClick={() => setTrue()}
-                      className='plus-button'
-                      size='50px'
-                      color='#6c63ff'
+                              </div>
+                            </th>
+                          </tr>
+                        )
+                    )}
+                  </tbody>
+                </table>
+              </div>
+            ) : (
+              <div className='no-student-container'>
+                <div className='no-student-wrapper'>
+                  <div className='no-student-img-wrapper'>
+                    <img
+                      className='no-student-img'
+                      src={noStudent}
+                      alt='no_students'
+                      onClick={() => {
+                        setTrue();
+                      }}
                     />
                   </div>
+                  <div className='no-student-header'>No Students Added</div>
+                  <div className='no-student-description'>
+                    Click the plus button to add a student
+                  </div>
                 </div>
-              )}
-            </div>
-          )}
+              </div>
+            )}
+            {show ? (
+              addStudent()
+            ) : (
+              <div className='plus-button-container'>
+                <div className='plus-button-wrapper'>
+                  <CgAdd
+                    onClick={() => setTrue()}
+                    className='plus-button'
+                    size='50px'
+                    color='#6c63ff'
+                  />
+                </div>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
