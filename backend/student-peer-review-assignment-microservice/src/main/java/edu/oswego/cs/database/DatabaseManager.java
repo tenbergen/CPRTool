3564bbe96f990716a0729a5da6cfd5e386cf7992
Mint <developer@chocomint.dev>
2022-09-17 14:22:56 +0700

    Reworked and simplified database connection (#304)

--- backend/student-peer-review-assignment-microservice/src/main/java/edu/oswego/cs/database/DatabaseManager.java
+++ backend/student-peer-review-assignment-microservice/src/main/java/edu/oswego/cs/database/DatabaseManager.java
@@ -10,21 +10,25 @@ import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class DatabaseManager {
-
     int studentPort = Integer.parseInt(System.getenv("MONGO_PORT"));
     int professorPort = Integer.parseInt(System.getenv("MONGO2_PORT"));
     int coursePort = Integer.parseInt(System.getenv("MONGO3_PORT"));
     int assignmentPort = Integer.parseInt(System.getenv("MONGO4_PORT"));
     int teamPort = Integer.parseInt(System.getenv("MONGO5_PORT"));
-    String hostname = System.getenv("MONGO_HOSTNAME");
+    boolean localhost = Boolean.parseBoolean(System.getenv("LOCALHOST"));
+    String host = "localhost";
     String mongoDatabase = System.getenv("MONGO_DATABASE");
     String mongoUser = System.getenv("MONGO_USERNAME");
     String mongoPassword = System.getenv("MONGO_PASSWORD");
 
+    public DatabaseManager() {
+    }
+
     public MongoDatabase getStudentDB() {
+        if (!localhost) host = "mongo";
         MongoCredential credentials = MongoCredential.createCredential(mongoUser, mongoDatabase, mongoPassword.toCharArray());
         MongoClient client = new MongoClient(
-                new ServerAddress(hostname, studentPort),
+                new ServerAddress(host, studentPort),
                 credentials,
                 new MongoClientOptions.Builder().build()
         );
@@ -32,9 +36,10 @@ public class DatabaseManager {
     }
 
     public MongoDatabase getProfessorDB() {
+        if (!localhost) host = "mongo2";
         MongoCredential credentials = MongoCredential.createCredential(mongoUser, mongoDatabase, mongoPassword.toCharArray());
         MongoClient client = new MongoClient(
-                new ServerAddress(hostname, professorPort),
+                new ServerAddress(host, professorPort),
                 credentials,
                 new MongoClientOptions.Builder().build()
         );
@@ -42,9 +47,10 @@ public class DatabaseManager {
     }
 
     public MongoDatabase getCourseDB() {
+        if (!localhost) host = "mongo3";
         MongoCredential credentials = MongoCredential.createCredential(mongoUser, mongoDatabase, mongoPassword.toCharArray());
         MongoClient client = new MongoClient(
-                new ServerAddress(hostname, coursePort),
+                new ServerAddress(host, coursePort),
                 credentials,
                 new MongoClientOptions.Builder().build()
         );
@@ -52,9 +58,10 @@ public class DatabaseManager {
     }
 
     public MongoDatabase getAssignmentDB() {
+        if (!localhost) host = "mongo4";
         MongoCredential credentials = MongoCredential.createCredential(mongoUser, mongoDatabase, mongoPassword.toCharArray());
         MongoClient client = new MongoClient(
-                new ServerAddress(hostname, assignmentPort),
+                new ServerAddress(host, assignmentPort),
                 credentials,
                 new MongoClientOptions.Builder().build()
         );
@@ -62,15 +69,13 @@ public class DatabaseManager {
     }
 
     public MongoDatabase getTeamDB() {
+        if (!localhost) host = "mongo5";
         MongoCredential credentials = MongoCredential.createCredential(mongoUser, mongoDatabase, mongoPassword.toCharArray());
         MongoClient client = new MongoClient(
-                new ServerAddress(hostname, teamPort),
+                new ServerAddress(host, teamPort),
                 credentials,
                 new MongoClientOptions.Builder().build()
         );
         return client.getDatabase(mongoDatabase);
-    }
-
-    public DatabaseManager() {
     }
 }
