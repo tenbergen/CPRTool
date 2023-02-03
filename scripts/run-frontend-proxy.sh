1020ef47cee4b45e9a4062a5344a2a54198030cd
daltamur <daltamur@oswego.edu>
2023-02-03 00:46:06 -0500

    Made a separate workflow for people working on the frontend so that they can easily develop for it without being tied to the docker-compose nginx web server

--- scripts/run-frontend-proxy.sh
+++ scripts/run-frontend-proxy.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# Run this script to only run the databases. Doing it this way is easier than having to make two database setups for localhost
-# a production environment
-
-#Use this script when running the microservices independently
-
 
 # Remove the previous instances.
 docker container stop cpr-frontend && docker container rm cpr-frontend
@@ -15,14 +10,9 @@ docker container stop cpr-peer-review-teams && docker container rm cpr-peer-revi
 docker container stop cpr-student-assignment && docker container rm cpr-student-assignment
 docker container stop cpr-student-peer-review-assignment && docker container rm cpr-student-peer-review-assignment
 docker container stop cpr-nginx && docker container rm cpr-nginx
-docker container stop cpr-mongo && docker container rm cpr-mongo
-docker container stop cpr-mongo2 && docker container rm cpr-mongo2
-docker container stop cpr-mongo3 && docker container rm cpr-mongo3
-docker container stop cpr-mongo4 && docker container rm cpr-mongo4
-docker container stop cpr-mongo5 && docker container rm cpr-mongo5
 
 # Back out one level to access docker-compose.yml.
 cd ..
 
-# Build the project using docker-compose and start the containers.
-docker-compose -f "independently-run-databases.yml" up -d --build
+# Build the project using docker-compose and start the container.
+docker-compose -f "docker-compose-local-frontend.yml" up -d --build
