63f27144efdeafda214a81e63828b5b23dacb269
PSchmitt1 <schmitpg@clarkson.edu>
2023-03-07 23:20:10 -0500

    commented out email domain requirements

--- frontend/src/main/frontend/src/components/ProfessorComponents/CoursesPage/ProfessorRosterComponent.js
+++ frontend/src/main/frontend/src/components/ProfessorComponents/CoursesPage/ProfessorRosterComponent.js
@@ -44,10 +44,10 @@ const ProfessorRosterComponent = () => {
     }
 
 
-     if (!Email.includes('oswego.edu')) {
-       alert('Please enter a valid Oswego email!');
-       return;
-     }
+     //if (!Email.includes('oswego.edu')) {
+     //  alert('Please enter a valid Oswego email!');
+     //  return;
+     //}
 
     const firstLastEmail = first + "-" + last + "-" + Email;
     const addStudentUrl = `${url}/${courseId}/students/${firstLastEmail}/add`;
