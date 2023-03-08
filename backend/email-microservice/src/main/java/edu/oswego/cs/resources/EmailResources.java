8dd05e2237dce6d837272875f9e293c02cd421aa
PSchmitt1 <schmitpg@clarkson.edu>
2023-03-08 03:07:54 -0500

    Changed testEmail yet again in a desperate attempt to get something working.

--- backend/email-microservice/src/main/java/edu/oswego/cs/resources/EmailResources.java
+++ backend/email-microservice/src/main/java/edu/oswego/cs/resources/EmailResources.java
@@ -3,21 +3,22 @@ package edu.oswego.cs.resources;
 import edu.oswego.cs.daos.AssignmentDAO;
 import edu.oswego.cs.daos.CourseDAO;
 import edu.oswego.cs.services.EmailService;
-import org.bson.Document;
 
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.List;
 
 public class EmailResources {
 
     EmailService emailService;
     @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     @Path("/testemail")
+    @RolesAllowed({"professor","student"})
     public Response testEmail() {
         emailService.assignmentCreatedEmail(new CourseDAO(), new AssignmentDAO());
-        return Response.status(Response.Status.OK).build();
+        return Response.status(Response.Status.CREATED).entity("Email Successfully Sent.").build();
     }
 }
