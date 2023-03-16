e508f49c8ca5561d7d7f4ddfc743b473728a8ecb
PSchmitt1 <schmitpg@clarkson.edu>
2023-03-16 19:31:14 -0400

    modified pathname in EmailService.java

--- backend/email-microservice/src/main/java/edu/oswego/cs/services/EmailService.java
+++ backend/email-microservice/src/main/java/edu/oswego/cs/services/EmailService.java
@@ -155,10 +155,7 @@ public class EmailService {
      */
     public void assignmentCreatedEmail(CourseDAO course, AssignmentDAO assignment) throws IOException {
         //load email body from template
-        BufferedReader reader = new BufferedReader(new FileReader(new File(".." + File.separator +
-                ".." + File.separator +
-                ".." + File.separator +
-                "templates"+ File.separator +
+        BufferedReader reader = new BufferedReader(new FileReader(new File(System.getProperty("user.dir") +
                 "assignmentCreatedEmail.html")));
         String line;
         StringBuilder stringBuilder = new StringBuilder();
