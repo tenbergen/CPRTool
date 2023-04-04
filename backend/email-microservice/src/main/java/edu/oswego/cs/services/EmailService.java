7df9be2d30126ee7511ec66681d9756033db1660
PSchmitt1 <schmitpg@clarkson.edu>
2023-04-03 22:50:02 -0400

    fixed cast error in gradeReceivedEmail

--- backend/email-microservice/src/main/java/edu/oswego/cs/services/EmailService.java
+++ backend/email-microservice/src/main/java/edu/oswego/cs/services/EmailService.java
@@ -221,7 +221,7 @@ public class EmailService {
         Document assignment = new AssignmentInterface().getSpecifiedAssignment(courseID, assignmentID);
         Document submission = new AssignmentInterface().getSubmission(assignmentID, teamID);
 
-        if(Integer.parseInt(submission.getString("grade")) != -1){ //may need to be changed as the grade system is updated.
+        if(submission.getInteger("grade") != -1){ //may need to be changed as the grade system is updated.
             return;
         }
 
