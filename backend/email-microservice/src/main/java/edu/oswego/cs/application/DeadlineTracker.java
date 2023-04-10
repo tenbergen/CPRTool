085ad578c969aedf5ce3c4804379e3b1980f451c
PSchmitt1 <schmitpg@clarkson.edu>
2023-04-10 11:17:48 -0400

    lengthened DeadlineTracker check() method schedule time cause I forgot to do that

--- backend/email-microservice/src/main/java/edu/oswego/cs/application/DeadlineTracker.java
+++ backend/email-microservice/src/main/java/edu/oswego/cs/application/DeadlineTracker.java
@@ -51,7 +51,7 @@ public class DeadlineTracker{
      * Calls EmailService.assignmentDeadlinePassed() or EmailService.peerReviewDeadlinePassed() when the respective case
      * comes up.
      */
-    @Schedule(second = "*/5", minute = "*", hour = "*", persistent = false)
+    @Schedule(second = "", minute = "*/5", hour = "*", persistent = false)
     public void check() {
         System.out.println("----------------------------------check"); //proves that this method is actually being run.
         if(assignments == null || reviews == null){
