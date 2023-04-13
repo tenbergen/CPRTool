dcb37495eb1d4c546f558f72a6dd9868a7bcfc03
Nathan <89611095+NULLWulf@users.noreply.github.com>
2023-04-13 18:56:50 -0400

    Update CourseInterface.java
    updated crn to course_id as parameters in blocked words in course interface.

--- backend/course-manager-microservice/src/main/java/edu/oswego/cs/database/CourseInterface.java
+++ backend/course-manager-microservice/src/main/java/edu/oswego/cs/database/CourseInterface.java
@@ -289,17 +289,17 @@ public class CourseInterface {
         }
     }
 
-    public void updateBlockedWordsForCourse(String crn, String payload) {
-        Document courseDocument = courseCollection.find(eq("crn", crn)).first();
+    public void updateBlockedWordsForCourse(String course_id, String payload) {
+        Document courseDocument = courseCollection.find(eq("course_id", course_id)).first();
         if (courseDocument == null) throw new CPRException(Response.Status.NOT_FOUND, "This course does not exist.");
         Gson gson = new Gson();
         List<String> blockedWords = gson.fromJson(payload, List.class);
         courseDocument.put("blocked_words", blockedWords);
-        courseCollection.updateOne(eq("crn", crn), set("blocked_words", blockedWords));
+        courseCollection.updateOne(eq("course_id", course_id), set("blocked_words", blockedWords));
     }
 
-    public String getBlockedWordsForCourse(String crn){
-        Document courseDocument = courseCollection.find(eq("crn", crn)).first();
+    public String getBlockedWordsForCourse(String course_id){
+        Document courseDocument = courseCollection.find(eq("course_id", course_id)).first();
         if (courseDocument == null) throw new CPRException(Response.Status.NOT_FOUND, "This course does not exist.");
         Gson gson = new Gson();
         List<String> blockedWords = courseDocument.getList("blocked_words", String.class);
