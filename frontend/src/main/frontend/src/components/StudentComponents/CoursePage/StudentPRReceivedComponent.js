d97a7a1db2c6d162b0d66723718ca517fdc29df8
Schmitt-Very-Cool <pschmitt@oswego.edu>
2023-04-16 19:17:05 -0400

    Trying out making my own frontend page

---
+++ frontend/src/main/frontend/src/components/StudentComponents/CoursePage/StudentPRReceivedComponent.js
@@ -0,0 +1,49 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import '../../styles/StudentAss.css';
+import { useParams } from 'react-router-dom';
+import { getCombinedAssignmentPeerReviews } from '../../../redux/features/assignmentSlice';
+import AssignmentTile from '../../AssignmentTile';
+import uuid from 'react-uuid';
+import noData from '../../../assets/no-data.png';
+import AssBarComponent from "../../AssBarComponent";
+import PRReceivedTile from "../../styles/PRReceivedTile";
+
+const StudentToDoComponent = () => {
+  const dispatch = useDispatch();
+  const store = useSelector((state) => state);
+  const { combinedAssignmentPeerReviews, assignmentsLoaded } =
+    store.assignments;
+  const { currentTeamId, teamLoaded } = store.teams;
+  const { courseId } = useParams();
+  const { lakerId } = store.auth;
+
+  useEffect(() => {
+    dispatch(
+      getCombinedAssignmentPeerReviews({ courseId, currentTeamId, lakerId })
+    );
+  }, [courseId, currentTeamId, lakerId, dispatch]);
+
+  return (
+    <h3>
+      {assignmentsLoaded && teamLoaded ? (
+        <div id='assList'>
+          {combinedAssignmentPeerReviews.length === 0 ? (
+            <div className='no-todo-wrapper'>
+              <img className='no-todo-img' src={noData} alt='No Todo' />
+              <div className='no-todo-head'>No assignments to show here</div>
+            </div>
+          ) : (
+            <div>
+              {combinedAssignmentPeerReviews.map((assignment) => (
+                <PRReceivedTile key={uuid()} assignment={assignment} />
+              ))}
+            </div>
+          )}
+        </div>
+      ) : null}
+    </h3>
+  );
+};
+
+export default StudentToDoComponent;
---
+++ frontend/src/main/frontend/src/pages/StudentPages/StudentPRReceivedPage.js
@@ -0,0 +1,87 @@
+import { useEffect, useState } from 'react';
+import './styles/StudentCourseStyle.css';
+import SidebarComponent from '../../components/SidebarComponent';
+import StudentTeamComponent from '../../components/StudentComponents/CoursePage/StudentTeamComponent';
+import StudentToDoComponent from '../../components/StudentComponents/CoursePage/StudentToDoComponent';
+import StudentSubmittedComponent from '../../components/StudentComponents/CoursePage/StudentSubmittedComponent';
+import { useLocation, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { getCourseDetailsAsync } from '../../redux/features/courseSlice';
+import { getCurrentCourseTeamAsync } from '../../redux/features/teamSlice';
+import MyTeamComponent from '../../components/StudentComponents/CoursePage/MyTeamComponent';
+import uuid from 'react-uuid';
+import NavigationContainerComponent
+  from "../../components/NavigationComponents/NavigationContainerComponent";
+import HeaderBar from "../../components/HeaderBar/HeaderBar";
+import StudentPRReceivedComponent from "../../components/StudentComponents/CoursePage/StudentPRReceivedComponent";
+
+const CourseComponent = ({ active, component, onClick }) => {
+  return (
+    <p
+      onClick={onClick}
+      className={
+        active
+          ? 'kumba-30 scp-component-link-clicked'
+          : 'kumba-30 scp-component-link'
+      }
+    >
+      {component}
+    </p>
+  );
+};
+
+function StudentCoursePage({chosen}) {
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const { courseId } = useParams();
+  const { lakerId } = useSelector((state) => state.auth);
+  const { currentTeamId, teamLoaded } = useSelector((state) => state.teams);
+
+  const [chosenComponent, setChosenComponent] = useState(chosen);
+
+  const components = ['To Do', 'Submitted', 'My Team'];
+
+  useEffect(() => {
+    dispatch(getCourseDetailsAsync(courseId));
+    dispatch(getCurrentCourseTeamAsync({ courseId, lakerId }));
+  }, [courseId, lakerId, dispatch]);
+
+  useEffect(() => {
+    setChosenComponent(chosen);
+  }, [chosen]);
+
+  return (
+    <div className="page-container">
+      <HeaderBar/>
+      <div className='scp-container'>
+        <NavigationContainerComponent/>
+        {/*<AssBarComponent />*/}
+        <div className='scp-component'>
+          {/* Not in a team yet */}
+          {teamLoaded && currentTeamId === null && <StudentTeamComponent />}
+
+          {/* Already in a team */}
+          {teamLoaded && currentTeamId !== null && (
+              <div>
+                <div className='scp-component-links'>
+                  {components.map((t) => (
+                      <CourseComponent
+                          key={uuid()}
+                          component={t}
+                          active={t === chosenComponent}
+                          onClick={() => setChosenComponent(t)}
+                      />
+                  ))}
+                </div>
+                <div>
+                  chosenComponent === 'To Do' && <StudentPRReceivedComponent />
+                </div>
+              </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default StudentCoursePage;
