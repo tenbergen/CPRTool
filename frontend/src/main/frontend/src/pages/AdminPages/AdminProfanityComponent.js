b60ef8d4b4963ef6ed69c7398fd3a0a9ec532d05
Dominic Altamura <daltamur@oswego.edu>
2023-04-18 12:19:41 -0400

    Admin int (#476)
    * professor profanity page finished all except for the 'Load Defaults' button
    
    * fixed backend update course route where updating a course would wipe the blocked words list
    
    * professor profanity settings now 100% completed
    
    * admin and professor profanity pages done

--- frontend/src/main/frontend/src/pages/AdminPages/AdminProfanityComponent.js
+++ frontend/src/main/frontend/src/pages/AdminPages/AdminProfanityComponent.js
@@ -1,28 +1,17 @@
-import '../../styles/EditCourse.css'
+import '../../components/styles/EditCourse.css'
 import { useDispatch, useSelector } from 'react-redux'
-import { useNavigate, useParams } from 'react-router-dom'
-import { getCourseDetailsAsync } from '../../../redux/features/courseSlice'
+import { useNavigate } from 'react-router-dom'
 import * as React from 'react'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
-function ProfessorProfanitySettingsComponent () {
-  const dispatch = useDispatch()
-  //use this course id value for doing the get and post requests
-  let { courseId } = useParams()
-  const updateProfanityUrl = `${process.env.REACT_APP_URL}/manage/professor/courses/` + courseId + `/profanity/update`
-  const getAdminProfanityUrl = `${process.env.REACT_APP_URL}/manage/admin/views/profanity`
+function AdminProfanityComponent () {
+  const updateProfanityUrl = `${process.env.REACT_APP_URL}/manage/admin/profanity/update`
   let navigate = useNavigate()
+
+  const getAdminProfanityUrl = `${process.env.REACT_APP_URL}/manage/admin/views/profanity`
   let profaninityArray = []
-  const { currentCourse } = useSelector((state) => state.courses)
-
-  useEffect(() => {
-    //set the profanity array value to whatever the current course has it as so long as it is not null
-    profaninityArray = currentCourse == null ? null : currentCourse.blocked_words
-    //set the text box input to the currently loaded bad words
-    setTextBoxInput(initialValueSet)
-  }, [currentCourse])
-
+  const [showModal, setShowModal] = useState(false)
   const initialValueSet = () => {
     //you would run this a lot like how the do in ProfessorEditAssignmentComponent
     let profanityText = profaninityArray[0]
@@ -37,8 +26,28 @@ function ProfessorProfanitySettingsComponent () {
     }
   }
   const [textboxInput, setTextBoxInput] = useState(initialValueSet)
-  const [showModal, setShowModal] = useState(false)
 
+  async function FetchDefaultProfanityList () {
+    let listVal = []
+    await axios
+      .get(getAdminProfanityUrl)
+      .then((res) => {
+        listVal = res.data
+      })
+      .catch((e) => {
+        console.error(e)
+        alert('Error getting global profanity list')
+      })
+    return listVal
+  }
+
+  useEffect(() => {
+    FetchDefaultProfanityList().then((result) => {
+        profaninityArray = result
+        setTextBoxInput(initialValueSet)
+      }
+    )
+  }, [])
 
   //modal object
   const Modal = () => {
@@ -49,8 +58,7 @@ function ProfessorProfanitySettingsComponent () {
           width: '45%'
         }}>
           <svg className="cross" style={{}} onClick={async () => {
-            navigate(-1)
-            dispatch(getCourseDetailsAsync(courseId))
+            window.location.reload()
           }}></svg>
           <span
             style={{
@@ -73,27 +81,6 @@ function ProfessorProfanitySettingsComponent () {
         </div>
       </div>
     )
-  }
-
-  const loadDefaults = async () => {
-    //you would run this a lot like how the do in ProfessorEditAssignmentComponent
-    await axios
-      .get(getAdminProfanityUrl)
-      .then((res) => {
-        let profanityText = res.data[0]
-        let defaultProfanityArray = res.data
-        defaultProfanityArray.forEach(element => {
-            if (element !== defaultProfanityArray[0]) {
-              profanityText += '\n' + element
-            }
-          }
-        )
-        setTextBoxInput(() => {return { profanityText } })
-      })
-      .catch((e) => {
-        console.error(e)
-        alert('Error updating profanity list')
-      })
   }
 
   const handleTextBoxChange = event => {
@@ -120,18 +107,9 @@ function ProfessorProfanitySettingsComponent () {
 
   return (
     <div className="ecc-form">
-      <h2
-        className="course-profanity-title">{currentCourse == null ? null : currentCourse.course_name} Profanity
-        Settings</h2>
-      <div>{' '}</div>
       <div className="course-profanity-details">
-        <p>Please enter words that you want to include in the profanity checking list for this course.
-          You can load a pre-created list of global profanity setting defaults by clicking the "Load Defaults"
-          button</p>
+        <p>Please enter words that you want to include in the global profanity checking list.</p>
         <p><i>(New words to be separated by a line, press "Enter" to add new words and "Delete" to remove)</i></p>
-        <button className="ecc-button-defaults" onClick={() => loadDefaults()}>
-          Load Defaults
-        </button>
         <div className="ecc-input-field">
           <form onSubmit={handleSubmit}>
                       <textarea value={textboxInput.profanityText} onChange={handleTextBoxChange} name="blockedWords"
@@ -142,9 +120,6 @@ function ProfessorProfanitySettingsComponent () {
               <button className="ecc-button" type="button" onClick={() => handleSubmit()}>Save
                 Changes <div>{showModal ? Modal() : null}</div></button>
             </form>
-            <button className="ecc-anchor-cancel" onClick={() => navigate(-1)}>
-              Go Back
-            </button>
           </div>
         </div>
 
@@ -153,4 +128,4 @@ function ProfessorProfanitySettingsComponent () {
   )
 }
 
-export default ProfessorProfanitySettingsComponent
+export default AdminProfanityComponent
