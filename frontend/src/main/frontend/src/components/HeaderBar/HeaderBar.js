382f78725a2498dc2cf2f1e62d9f910a22607489
Dominic Altamura <daltamur@oswego.edu>
2023-04-20 15:57:20 -0400

    Admin front back int (#491)
    * professor profanity page finished all except for the 'Load Defaults' button
    
    * fixed backend update course route where updating a course would wipe the blocked words list
    
    * professor profanity settings now 100% completed
    
    * admin and professor profanity pages done
    
    * Admin int (#476)
    
    * professor profanity page finished all except for the 'Load Defaults' button
    
    * fixed backend update course route where updating a course would wipe the blocked words list
    
    * professor profanity settings now 100% completed
    
    * admin and professor profanity pages done
    
    * centered success modal
    
    * Fixed success modal on admin interface (#477)
    
    * professor profanity page finished all except for the 'Load Defaults' button
    
    * fixed backend update course route where updating a course would wipe the blocked words list
    
    * professor profanity settings now 100% completed
    
    * admin and professor profanity pages done
    
    * centered success modal
    
    * fixed admin button in header bar where if you go from student to admin you get an unauthorized error
    
    * modified the header bar so that the admin option only shows up if the user has the admin role
    
    * used a library to decode jwt now instead of manually decoding the base64 myself
    
    ---------
    
    Co-authored-by: Kayyali <89478559+Kayyali78@users.noreply.github.com>

--- frontend/src/main/frontend/src/components/HeaderBar/HeaderBar.js
+++ frontend/src/main/frontend/src/components/HeaderBar/HeaderBar.js
@@ -2,10 +2,13 @@ import './HeaderBar.css'
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import jwt_decode from 'jwt-decode'
 
 const HeaderBar = () => {
   const role = useSelector((state) => state.auth.role)
   const alt_role = localStorage.getItem('alt_role')
+  const admin_access_token = jwt_decode(localStorage.getItem('jwt_token')).groups.includes('admin')
+
   let navigate = useNavigate()
 
   function showDropdown () {
@@ -23,6 +26,13 @@ const HeaderBar = () => {
     localStorage.removeItem('alt_role')
     //since the professor can change views from any page now, it is necessary to redirect them back to the route url
     navigate('/')
+    window.location.reload(false)
+  }
+
+  const adminView = () => {
+    localStorage.removeItem('alt_role')
+    //since the professor can change views from any page now, it is necessary to redirect them back to the route url
+    navigate('/professor/admin')
     window.location.reload(false)
   }
 
@@ -54,9 +64,7 @@ const HeaderBar = () => {
           <div className="admin-dropdown">
             <button id="admin-button" onClick={() => showDropdown()}></button>
             <div id="dropdown-options" className="dropdown-content">
-              <Link to={`/professor/admin`}>
-                Admin
-              </Link>
+              {admin_access_token ? <a href="#" onClick={adminView}>Admin</a> : null}
               <a href="#" onClick={professorView}>Instructor</a>
               <a href="#" onClick={studentView}>Student</a>
             </div>
