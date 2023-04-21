ae5cafb5128e16cd07257667cfb953b9f3f812b5
Kayyali <89478559+Kayyali78@users.noreply.github.com>
2023-04-21 19:12:51 -0400

    Revert "G UI"

--- frontend/src/main/frontend/src/routing/RouterHandler.js
+++ frontend/src/main/frontend/src/routing/RouterHandler.js
@@ -42,7 +42,7 @@ const RouterHandler = () => {
               element={<ProfessorGradesPage/>}
             />
             <Route
-              path="professor/:courseId/assignments/create/assignment"
+              path="professor/:courseId/create/assignment"
               element={<CreateAssignmentPage/>}
             />
             <Route
