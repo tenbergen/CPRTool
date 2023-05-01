d991e73efa91491897177e54e84799223f4707a3
MatthewDBrown50 <matthew.d.brown50@gmail.com>
2023-05-01 17:22:42 -0400

    Added Product Owners and Project Lead sections to CurrentContributorsComponent.js, and moved sections around to balance the column lengths.

--- frontend/src/main/frontend/src/pages/AboutPage/AboutPageComponents/CurrentContributorsComponent.js
+++ frontend/src/main/frontend/src/pages/AboutPage/AboutPageComponents/CurrentContributorsComponent.js
@@ -4,8 +4,31 @@ import { Link } from 'react-router-dom';
 
 const CurrentContributorsComponent = () => {
     return (
-        <div className='current-contributors-component-container'>
+        <div className='current-contributors-component-container' style={{marginLeft: '6%'}}>
             <div className='current-contributors-column' style={{ marginTop: '-10px' }}>
+                <div className='current-contributors-icon-container'>
+                    <div className='current-contributors-product-owners-icon' />
+                </div>
+                <div>
+                    <p className='inter-20-bold' style={{ margin: '20px 0 8px 0' }}>
+                        Product Owners:
+                    </p>
+                </div>
+                <div>
+                    <ul className='inter-20-medium' style={{ listStyleType: 'disc' }}>
+                        <div className='current-contributors-name-container'>
+                            <Link to='https://uxfol.io/83234f5b' className='inter-20-bold-blue'>
+                                <li>Andjela Djapa</li>
+                            </Link>
+                        </div>
+                        <div className='current-contributors-name-container'>
+                            <Link to='https://www.linkedin.com/in/dannielle-k-32591b18a/'
+                                  className='inter-20-bold-blue'>
+                                <li>Dannielle Kline</li>
+                            </Link>
+                        </div>
+                    </ul>
+                </div>
                 <div className='current-contributors-icon-container'>
                     <div className='current-contributors-engine-icon' />
                 </div>
@@ -60,6 +83,113 @@ const CurrentContributorsComponent = () => {
                     </ul>
                 </div>
                 <div className='current-contributors-icon-container'>
+                    <div className='current-contributors-gui-icon' />
+                </div>
+                <div>
+                    <p className='inter-20-bold' style={{ margin: '20px 0 8px 0' }}>
+                        GUI:
+                    </p>
+                    <ul className='inter-20-medium' style={{ listStyleType: 'disc' }}>
+                        <div className='current-contributors-name-container'>
+                            <Link to='https://github.com/markabbe' className='inter-20-bold-blue'>
+                                <li>Mark Abbe</li>
+                            </Link>
+                        </div>
+                        <div className='current-contributors-name-container'>
+                            <Link to='https://www.linkedin.com/in/snigbehara/' className='inter-20-bold-blue'>
+                                <li>Snigdha Behara</li>
+                            </Link>
+                        </div>
+                        <div className='current-contributors-name-container'>
+                            <Link to='https://www.linkedin.com/in/jbetanco' className='inter-20-bold-blue'>
+                                <li>Joel Betancourt</li>
+                            </Link>
+                        </div>
+                        <div className='current-contributors-name-container'>
+                            <Link to='https://github.com/MatthewDBrown50' className='inter-20-bold-blue'>
+                                <li>Matthew D Brown</li>
+                            </Link>
+                        </div>
+                        <div className='current-contributors-name-container'>
+                            <Link to='https://www.linkedin.com/in/benjamin-melby-9b61a3267/'
+                                  className='inter-20-bold-blue'>
+                                <li>Benjamin Melby</li>
+                            </Link>
+                        </div>
+                        <div className='current-contributors-name-container'>
+                            <Link to='https://github.com/rshtikya' className='inter-20-bold-blue'>
+                                <li>Raffi Shtikyan</li>
+                            </Link>
+                        </div>
+                    </ul>
+                </div>
+            </div>
+            <div className='current-contributors-column'>
+                <div className='current-contributors-icon-container'>
+                    <div className='current-contributors-db-icon' />
+                </div>
+                <div>
+                    <p className='inter-20-bold' style={{ margin: '20px 0 8px 0' }}>
+                        Database and Networking:
+                    </p>
+                    <ul className='inter-20-medium' style={{ listStyleType: 'disc' }}>
+                        <div className='current-contributors-name-container'>
+                            <Link to='https://www.linkedin.com/in/giovanni-anastasio-399579190/'
+                                  className='inter-20-bold-blue'>
+                                <li>Giovanni Anastasio</li>
+                            </Link>
+                        </div>
+                        <div className='current-contributors-name-container'>
+                            <Link to='https://www.linkedin.com/in/dannielle-k-32591b18a/'
+                                  className='inter-20-bold-blue'>
+                                <li>Dannielle Kline</li>
+                            </Link>
+                        </div>
+                        <div className='current-contributors-name-container'>
+                            <Link to='https://uxfol.io/home/portfolios/BaharehNejati' className='inter-20-bold-blue'>
+                                <li>Bahareh Nejati</li>
+                            </Link>
+                        </div>
+                        <div className='current-contributors-name-container'>
+                            <Link to='https://www.linkedin.com/in/nwolf2/' className='inter-20-bold-blue'>
+                                <li>Nathaniel Wolf</li>
+                            </Link>
+                        </div>
+                    </ul>
+                </div>
+                <div className='current-contributors-icon-container'>
+                    <div className='current-contributors-req-icon' />
+                </div>
+                <div>
+                    <p className='inter-20-bold' style={{ margin: '20px 0 8px 0' }}>
+                        Requirements:
+                    </p>
+                    <ul className='inter-20-medium' style={{ listStyleType: 'disc' }}>
+                        <div className='current-contributors-name-container'>
+                            <Link to='http://cs.oswego.edu/~fcamach2/Portfolio%20Website/'
+                                  className='inter-20-bold-blue'>
+                                <li>Franklin Camacho III</li>
+                            </Link>
+                        </div>
+                        <div className='current-contributors-name-container'>
+                            <Link to='https://www.linkedin.com/in/jake-hoffman-03b447154/'
+                                  className='inter-20-bold-blue'>
+                                <li>Jake Hoffman</li>
+                            </Link>
+                        </div>
+                        <div className='current-contributors-name-container'>
+                            <Link to='https://claratribunella.com ' className='inter-20-bold-blue'>
+                                <li>Clara Tribunella</li>
+                            </Link>
+                        </div>
+                        <div className='current-contributors-name-container'>
+                            <Link to='https://github.com/Eiskwean' className='inter-20-bold-blue'>
+                                <li>Sarah Wong</li>
+                            </Link>
+                        </div>
+                    </ul>
+                </div>
+                <div className='current-contributors-icon-container'>
                     <div className='current-contributors-qa-icon' />
                 </div>
                 <div>
@@ -105,47 +235,6 @@ const CurrentContributorsComponent = () => {
                 </div>
             </div>
             <div className='current-contributors-column'>
-                <div className='current-contributors-icon-container'>
-                    <div className='current-contributors-gui-icon' />
-                </div>
-                <div>
-                    <p className='inter-20-bold' style={{ margin: '20px 0 8px 0' }}>
-                        GUI:
-                    </p>
-                    <ul className='inter-20-medium' style={{ listStyleType: 'disc' }}>
-                        <div className='current-contributors-name-container'>
-                            <Link to='https://github.com/markabbe' className='inter-20-bold-blue'>
-                                <li>Mark Abbe</li>
-                            </Link>
-                        </div>
-                        <div className='current-contributors-name-container'>
-                            <Link to='https://www.linkedin.com/in/snigbehara/' className='inter-20-bold-blue'>
-                                <li>Snigdha Behara</li>
-                            </Link>
-                        </div>
-                        <div className='current-contributors-name-container'>
-                            <Link to='https://www.linkedin.com/in/jbetanco' className='inter-20-bold-blue'>
-                                <li>Joel Betancourt</li>
-                            </Link>
-                        </div>
-                        <div className='current-contributors-name-container'>
-                            <Link to='https://github.com/MatthewDBrown50' className='inter-20-bold-blue'>
-                                <li>Matthew D Brown</li>
-                            </Link>
-                        </div>
-                        <div className='current-contributors-name-container'>
-                            <Link to='https://www.linkedin.com/in/benjamin-melby-9b61a3267/'
-                                  className='inter-20-bold-blue'>
-                                <li>Benjamin Melby</li>
-                            </Link>
-                        </div>
-                        <div className='current-contributors-name-container'>
-                            <Link to='https://github.com/rshtikya' className='inter-20-bold-blue'>
-                                <li>Raffi Shtikyan</li>
-                            </Link>
-                        </div>
-                    </ul>
-                </div>
                 <div className='current-contributors-icon-container'>
                     <div className='current-contributors-usability-icon' />
                 </div>
@@ -220,100 +309,45 @@ const CurrentContributorsComponent = () => {
                         </div>
                     </ul>
                 </div>
-
-
-            </div>
-            <div className='current-contributors-column'>
                 <div className='current-contributors-icon-container'>
-                    <div className='current-contributors-db-icon' />
+                    <div className='current-contributors-ibm-icon' />
                 </div>
                 <div>
                     <p className='inter-20-bold' style={{ margin: '20px 0 8px 0' }}>
-                        Database and Networking:
+                        IBM Project Liaison:
                     </p>
                     <ul className='inter-20-medium' style={{ listStyleType: 'disc' }}>
                         <div className='current-contributors-name-container'>
-                            <Link to='https://www.linkedin.com/in/giovanni-anastasio-399579190/'
-                                  className='inter-20-bold-blue'>
-                                <li>Giovanni Anastasio</li>
-                            </Link>
+                            <li>Paul Austin</li>
                         </div>
                         <div className='current-contributors-name-container'>
-                            <Link to='https://www.linkedin.com/in/dannielle-k-32591b18a/'
-                                  className='inter-20-bold-blue'>
-                                <li>Dannielle Kline</li>
-                            </Link>
+                            <li>Rumana Haque</li>
                         </div>
                         <div className='current-contributors-name-container'>
-                            <Link to='https://uxfol.io/home/portfolios/BaharehNejati' className='inter-20-bold-blue'>
-                                <li>Bahareh Nejati</li>
-                            </Link>
+                            <li>Adam Wisniewski</li>
                         </div>
                         <div className='current-contributors-name-container'>
-                            <Link to='https://www.linkedin.com/in/nwolf2/' className='inter-20-bold-blue'>
-                                <li>Nathaniel Wolf</li>
-                            </Link>
+                            <li>Adam Yoho</li>
                         </div>
                     </ul>
                 </div>
                 <div className='current-contributors-icon-container'>
-                    <div className='current-contributors-req-icon' />
+                    <div className='current-contributors-project-lead-icon' />
                 </div>
                 <div>
                     <p className='inter-20-bold' style={{ margin: '20px 0 8px 0' }}>
-                        Requirements:
+                        Project Lead:
                     </p>
+                </div>
+                <div>
                     <ul className='inter-20-medium' style={{ listStyleType: 'disc' }}>
                         <div className='current-contributors-name-container'>
-                            <Link to='http://cs.oswego.edu/~fcamach2/Portfolio%20Website/'
-                                  className='inter-20-bold-blue'>
-                                <li>Franklin Camacho III</li>
-                            </Link>
-                        </div>
-                        <div className='current-contributors-name-container'>
-                            <Link to='https://www.linkedin.com/in/jake-hoffman-03b447154/'
-                                  className='inter-20-bold-blue'>
-                                <li>Jake Hoffman</li>
-                            </Link>
-                        </div>
-                        <div className='current-contributors-name-container'>
-                            <Link to='https://claratribunella.com ' className='inter-20-bold-blue'>
-                                <li>Clara Tribunella</li>
-                            </Link>
-                        </div>
-                        <div className='current-contributors-name-container'>
-                            <Link to='https://github.com/Eiskwean' className='inter-20-bold-blue'>
-                                <li>Sarah Wong</li>
+                            <Link to='http://cs.oswego.edu/~tenberge/' className='inter-20-bold-blue'>
+                                <li>Prof. Dr. Bastian Tenbergen</li>
                             </Link>
                         </div>
                     </ul>
                 </div>
-                <div className='current-contributors-icon-container'>
-                    <div className='current-contributors-stakeholder-icon' />
-                </div>
-                <div>
-                    <p className='inter-20-bold' style={{ margin: '20px 0 8px 0' }}>
-                        Stakeholders:
-                    </p>
-                    <ul className='inter-20-medium' style={{ listStyleType: 'disc' }}>
-                        <div className='current-contributors-name-container'>
-                            <li>Paul Austin, IBM</li>
-                        </div>
-                        <div className='current-contributors-name-container'>
-                            <li>Rumana Haque, IBM</li>
-                        </div>
-                        <div className='current-contributors-name-container'>
-                            <li>Bastian Tenbergen</li>
-                        </div>
-                        <div className='current-contributors-name-container'>
-                            <li>Adam Wisniewski, IBM</li>
-                        </div>
-                        <div className='current-contributors-name-container'>
-                            <li>Adam Yoho, IBM</li>
-                        </div>
-                    </ul>
-                </div>
-
             </div>
         </div>
     );
